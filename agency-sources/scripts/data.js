b27fb1cbeb7843fc8291bdb362a6d8a82044e298
Patrick_B <pb5191@gmail.com>
2020-03-16 17:53:02 -0400

    refactoring with data object.

--- agency-sources/scripts/data.js
+++ agency-sources/scripts/data.js
@@ -1,5 +1,22 @@
-    const agencyDataFund =  [
-                    {"ID":"10481","AgencyID":"PRA","Amount":"","Purpose":"","FundNumber":"","FundStart":"7/1/2019 12:00:00 AM","FundEnd":"6/30/2020 12:00:00 AM"},
-                    {"ID":"10482","AgencyID":"PRA","Amount":"","Purpose":"RS  Test","FundNumber":"","FundStart":"7/1/2019 12:00:00 AM","FundEnd":"6/30/2020 12:00:00 AM"}
-                ];
+const agencyDataFund = [
+  {
+    ID: "10481",
+    AgencyID: "PRA",
+    Amount: "",
+    Purpose: "",
+    FundNumber: "",
+    FundStart: "7/1/2019 12:00:00 AM",
+    FundEnd: "6/30/2020 12:00:00 AM"
+  },
+  {
+    ID: "10482",
+    AgencyID: "PRA",
+    Amount: "100000",
+    Purpose: "RS  Test",
+    FundNumber: "",
+    FundStart: "7/1/2019 12:00:00 AM",
+    FundEnd: "6/30/2020 12:00:00 AM"
+  }
+];
 
+const CurrentFY = "2020";
