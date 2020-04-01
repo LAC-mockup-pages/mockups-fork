1a5ceedcc84c5719389daffbe5bed86b07ba0f3d
Patrick_B <pb5191@gmail.com>
2020-04-01 10:52:07 -0400

    added category id to data.js objects

--- agency-litzone-outcomes/scripts/data.js
+++ agency-litzone-outcomes/scripts/data.js
@@ -1,14 +1,14 @@
         const outcomesData = [
-                    { "ID": "1009", "AgencyID": "PRA", "Category": "Health Literacy", "Description": "Test 12.1 a" },
-                    { "ID": "1010", "AgencyID": "PRA", "Category": "Functional Literacy", "Description": "Wrote down a family recipe" },
-                    { "ID": "1265", "AgencyID": "PRA", "Category": "Community Outcomes", "Description": "Joined a club" },
-                    { "ID": "1355", "AgencyID": "PRA", "Category": "School Relations", "Description": "Sit for GED test" },
-                    { "ID": "2694", "AgencyID": "PRA", "Category": "Health Literacy", "Description": "Get Flu Vaccination" },
-                    { "ID": "2881", "AgencyID": "PRA", "Category": "Social Services", "Description": "TASC Test" },
-                    { "ID": "2967", "AgencyID": "PRA", "Category": "Health Literacy", "Description": "applied for health benefits (self)" },
-                    { "ID": "3231", "AgencyID": "PRA", "Category": "Workforce Readiness", "Description": "Dress for Success" },
-                    { "ID": "3232", "AgencyID": "PRA", "Category": "Family Literacy", "Description": "get a library card" },
-                    { "ID": "3252", "AgencyID": "PRA", "Category": "Community Outcomes", "Description": "Volunteering for meals on wheels" },
+                    { "ID": "1009", "AgencyID": "PRA", "OutcomeSortOrder": "1", "Category": "Health Literacy", "Description": "Test 12.1 a" },
+                    { "ID": "1010", "AgencyID": "PRA", "OutcomeSortOrder": "3", "Category": "Functional Literacy", "Description": "Wrote down a family recipe" },
+                    { "ID": "1265", "AgencyID": "PRA", "OutcomeSortOrder": "5", "Category": "Community Outcomes", "Description": "Joined a club" },
+                    { "ID": "1355", "AgencyID": "PRA", "OutcomeSortOrder": "6", "Category": "School Relations", "Description": "Sit for GED test" },
+                    { "ID": "2694", "AgencyID": "PRA", "OutcomeSortOrder": "1", "Category": "Health Literacy", "Description": "Get Flu Vaccination" },
+                    { "ID": "2881", "AgencyID": "PRA", "OutcomeSortOrder": "7", "Category": "Social Services", "Description": "TASC Test" },
+                    { "ID": "2967", "AgencyID": "PRA", "OutcomeSortOrder": "1", "Category": "Health Literacy", "Description": "applied for health benefits (self)" },
+                    { "ID": "3231", "AgencyID": "PRA", "OutcomeSortOrder": "8", "Category": "Workforce Readiness", "Description": "Dress for Success" },
+                    { "ID": "3232", "AgencyID": "PRA", "OutcomeSortOrder": "4", "Category": "Family Literacy", "Description": "get a library card" },
+                    { "ID": "3252", "AgencyID": "PRA", "OutcomeSortOrder": "5", "Category": "Community Outcomes", "Description": "Volunteering for meals on wheels" },
         ];
 
         const categoryData = [
