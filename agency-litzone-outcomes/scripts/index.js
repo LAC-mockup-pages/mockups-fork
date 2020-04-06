ad6686b4a4269ac5913dd71777754081cfa3e790
Patrick_B <pb5191@gmail.com>
2020-04-06 09:19:42 -0400

    added process new Outcome

--- agency-litzone-outcomes/scripts/index.js
+++ agency-litzone-outcomes/scripts/index.js
@@ -1,148 +1,59 @@
 // Actions and logic
-const agencyData = [
-  {
-    id: 1,
-    CategoryName: "Social Services",
-    Descriptions: [
-      { Text: "Operative tertiary application" },
-      { Text: "Organic foreground task-force" },
-      { Text: "User-friendly optimal orchestration" },
-      { Text: "Switchable 24 hour Graphic Interface" }
-    ]
-  },
-  {
-    id: 2,
-    CategoryName: "Functional Literacy",
-    Descriptions: [{ Text: "Reactive real-time pricing structure" }]
-  },
-  {
-    id: 3,
-    CategoryName: "Community Outcomes",
-    Descriptions: [
-      { Text: "Enhanced encompassing toolset" },
-      { Text: "Synchronised reciprocal approach" }
-    ]
-  },
-  {
-    id: 4,
-    CategoryName: "School Relations",
-    Descriptions: [
-      { Text: "Vision-oriented radical help-desk" },
-      { Text: "Optional well-modulated help-desk" },
-      { Text: "Distributed asymmetric data-warehouse" },
-      { Text: "Monitored regional productivity" }
-    ]
-  },
-  {
-    id: 5,
-    CategoryName: "Health Literacy",
-    Descriptions: [
-      { Text: "Enterprise-wide foreground strategy" },
-      { Text: "Networked next generation capacity" },
-      { Text: "Streamlined 5th generation installation" }
-    ]
-  },
-  {
-    id: 6,
-    CategoryName: "Workforce Readiness",
-    Descriptions: [
-      { Text: "Self-enabling next generation open architecture" },
-      { Text: "Diverse even-keeled leverage" },
-      { Text: "Assimilated composite info-mediaries" },
-      { Text: "Fully-configurable exuding synergy" }
-    ]
-  },
-  {
-    id: 7,
-    CategoryName: "Financial Literacy",
-    Descriptions: [
-      { Text: "Fundamental even-keeled database" },
-      { Text: "Upgradable heuristic middleware" },
-      { Text: "Distributed multi-state internet solution" },
-      { Text: "Assimilated high-level application" }
-    ]
-  },
-  {
-    id: 8,
-    CategoryName: "Legal Services",
-    Descriptions: [
-      { Text: "Decentralized empowering flexibility" },
-      { Text: "Reverse-engineered cohesive challenge" },
-      { Text: "Down-sized upward-trending superstructure" },
-      { Text: "De-engineered uniform architecture" }
-    ]
-  },
-  {
-    id: 9,
-    CategoryName: "Citizenship",
-    Descriptions: [
-      { Text: "Advanced value-added intranet" },
-      { Text: "Public-key zero administration implementation" },
-      { Text: "Right-sized context-sensitive intranet" }
-    ]
-  }
-];
 
 const data = outcomesData;
 const categories = categoryData;
 
-const placeholderList = ["id", "Category", "Description"];
+const createNewRecord = (list) => {
+  const optionList = list.map(
+    (item) =>
+      `<option class="blue-text" value='${item.OutcomeSortOrder}'>
+        ${item.Category}</option>`
+  );
 
-const headerList = ["id", "Category", "Description"];
-
-const labelList = ["id", "Category", "Description"];
-
-const categoryList = agencyData
-  .sort((a, b) => (a.CategoryName > b.CategoryName ? 1 : -1))
-  .map(item => [item.id, item.CategoryName]);
-
-const createNewRecord = () => {
-  let optionList =
-    "<option class='red-text' disabled selected>Select a Category</option>";
-  for (let category of categoryList) {
-    optionList += `<option class="blue-text" value='${category[0]}'>${category[1]}</option>`;
-  }
+  const agency = $(".table-body").attr("id");
 
   $("#new-outcome").append(
-    `<select id="new-select" form="new-outcome" class="form-control red-text">${optionList}</select>
-        <input type="text" id="input-new-outcome" class="form-control blue-text" placeholder="Description" spellcheck="true" required>
-    <button type="submit" id="submit-btn" class="btn btn-primary">Add</button>
+    `<select id="new-select" form="new-outcome" class="form-control red-text">
+        <option class='red-text' disabled selected>Select a Category</option>
+        ${optionList}</select>
+      <input type="text" id="input-new-outcome" class="form-control blue-text" placeholder="Description" spellcheck="true" required>
+    <button type="button" id="submit-btn" form="new-outcome" class="btn btn-primary">Add</button>
     <button type="button" id="cancel-btn" class="btn btn-default">Cancel</button>`
   );
 };
 
-const createDataList = list => {
+const createDataList = (list) => {
   const catList = [];
   for (const item of list) {
     if (!catList.includes(item.OutcomeSortOrder)) {
       catList.push(item.OutcomeSortOrder);
     }
   }
-  const dataList = catList.map(num => {
+  const dataList = catList.map((num) => {
     const category = categoryData.filter(
-      item => item.OutcomeSortOrder === num
+      (item) => item.OutcomeSortOrder === num
     )[0].Category;
     const outcomes = data
-      .filter(item => item.OutcomeSortOrder === num)
-      .map(item => [item.ID, item.Description]);
+      .filter((item) => item.OutcomeSortOrder === num)
+      .map((item) => [item.ID, item.Description]);
     return { cat: category, catId: num, outcomes: outcomes };
   });
   return dataList;
 };
 
-const createHeader = list => {
-  const result = list.map(item => `<th>${item}</th>`).join("");
+const createHeader = (list) => {
+  const result = list.map((item) => `<th>${item}</th>`).join("");
   return `<thead>${result}</thead>`;
 };
 
-const createBody = list => {
+const createBody = (list) => {
   const dataList = createDataList(list);
   const agId = data[0].AgencyID;
 
   const rows = dataList
-    .map(item => {
+    .map((item) => {
       const values = item.outcomes
-        .map(arr => {
+        .map((arr) => {
           return `<div class="inside-value" id=${arr[0]} title="Click to edit" data-catid=${item.catId}>${arr[1]}</div>`;
         })
         .join("");
@@ -156,7 +67,7 @@ const createBody = list => {
   return `<tbody class="table-body" id=${agId}>${rows}</tbody>`;
 };
 
-const createView = list => {
+const createView = (list) => {
   const headerValues = Object.keys(list[0]).slice(3);
   const tableHead = createHeader(headerValues);
   const orderedList = list.sort((a, b) => (a.Category > b.Category ? 1 : -1));
@@ -181,14 +92,12 @@ const mergeHashToObject = (hashTable, obj) => {
   return obj;
 };
 
-const saveMods = form => {
+const saveMods = (form) => {
   const submittedData = $(form).serializeArray();
   console.log("submittedData :", submittedData);
 
   const keys = ["ID", "AgencyID"];
-  const val = $(form)
-    .attr("data-identifier")
-    .split("-");
+  const val = $(form).attr("data-identifier").split("-");
   const firstStep = mergeArraysToObject(keys, val);
   const result = mergeHashToObject(submittedData, firstStep);
 
@@ -202,7 +111,7 @@ const saveMods = form => {
 
 $(document).ready(() => {
   // * sub-navbar/index.js
-  $("#sub-nav li").click(function() {
+  $("#sub-nav li").click(function () {
     $("#sub-nav li").removeClass("blue-light-bg blue-text");
     $(this).toggleClass("blue-light-bg blue-text");
   });
@@ -215,29 +124,48 @@ $(document).ready(() => {
         ? "inline-block"
         : "none";
   });
-  btnToTop.click(e => {
+  btnToTop.click((e) => {
     e.preventDefault();
     $("html, body").animate({ scrollTop: 0 }, "600");
   });
 
   // * data viewing
-  createNewRecord();
+  createNewRecord(categories);
   $("#view-bloc").append(createView(data));
 
   //* Adding a new outcome
-  $("#new-select").change(function() {
-    const selectedOption = Number($(this).val());
-    const selectedObj = agencyData.filter(obj => obj.id === selectedOption);
-    $(".table-body").empty();
-    viewData(selectedObj);
+  $("#new-select").change(function () {
+    const selectedOption = $(this).val();
+    const row = $(`#${selectedOption}`).get();
+    $(".table-body").empty().append(row);
 
     $("#cancel-btn").click(() => {
       location.reload();
     });
+    $("#submit-btn").click(function (evnt) {
+      evnt.preventDefault();
+      evnt.stopPropagation();
+      const form = `#${$(this).attr("form")}`;
+      const agencyId = $("table-body").attr("id");
+      const newCategory = $(form + ">select").val();
+      const newDescription = $(form + ">input").val();
+      const result = {
+        AgencyID: agencyId,
+        Category: newCategory,
+        Description: newDescription,
+      };
+
+      //! =================================================
+      //! JSON Object to send back to database
+      console.log("result :", JSON.stringify(result));
+      //! =================================================
+
+      //ToDO Reloading/resetting with new data
+    });
   });
 
   //* Select outcome
-  $("[title^='Click']").click(function(evnt) {
+  $("[title^='Click']").click(function (evnt) {
     evnt.preventDefault();
     evnt.stopPropagation();
 
@@ -246,9 +174,10 @@ $(document).ready(() => {
     const identifier = `${$(this).attr("id")}-${$("tbody").attr("id")}`;
 
     const optionList = categories
-      .map(item => {
+      .map((item) => {
         const selected = item.OutcomeSortOrder === catId ? " selected" : "";
-        return `<option value=${item.OutcomeSortOrder}${selected}>${item.Category}</option>`;
+        return `<option value=${item.OutcomeSortOrder}${selected}>
+        ${item.Category}</option>`;
       })
       .join("");
 
@@ -272,7 +201,7 @@ $(document).ready(() => {
   });
 
   //* Saving mods after editing selected outcome
-  $("#save-btn").click(function(evnt) {
+  $("#save-btn").click(function (evnt) {
     evnt.preventDefault();
     evnt.stopPropagation();
     const form = `#${$(this).attr("form")}`;
