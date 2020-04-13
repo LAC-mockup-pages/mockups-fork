1b1bb58dfab4d0e6a357eb30bdc6049788f7dd15
GAJacques <60234143+GAJacques@users.noreply.github.com>
2020-04-13 12:17:03 -0400

    Add files via upload

---
+++ agency-personnel/scripts/data-GetPersInstHours.js
@@ -0,0 +1,11 @@
+
+GetPersIsntHours = 
+[{"ID":"2256","PersonnelID":"PorterSamanthaPRA9","ClassID":"PRABE6CFY2020_W213.0","InstHours":"0","TestHours":"0","TestContHours":"0","LabHours":"0","ExtraHoursLT12":"0"}]
+
+
+GetReportingPeriods = 
+[{"PeriodID":"PRA20190701","Period":"07/01/2019 - 07/31/2019"},
+{"PeriodID":"PRA20190801","Period":"08/01/2019 - 08/31/2019"},
+{"PeriodID":"PRA20190901","Period":"09/01/2019 - 09/30/2019"},
+{"PeriodID":"PRA20191001","Period":"10/01/2019 - 10/31/2019"}]
+
---
+++ agency-personnel/scripts/data-GetStatusHistory.js
@@ -0,0 +1,13 @@
+
+GetPersStatusHistory = [{"ID":"121368","PersonnelID":"PorterSamanthaPRA9","PersStatusDate":"12/19/2019","PersonnelStatID":"C","PersonnelStatDesc":"Active"},{"ID":"121367","PersonnelID":"PorterSamanthaPRA9","PersStatusDate":"12/18/2019","PersonnelStatID":"L","PersonnelStatDesc":"Adult Education Certification"},{"ID":"118131","PersonnelID":"PorterSamanthaPRA9","PersStatusDate":"02/02/2019","PersonnelStatID":"C","PersonnelStatDesc":"Active"},{"ID":"118130","PersonnelID":"PorterSamanthaPRA9","PersStatusDate":"02/01/2019","PersonnelStatID":"L","PersonnelStatDesc":"Adult Education Certification"},{"ID":"11900","PersonnelID":"PorterSamanthaPRA9","PersStatusDate":"07/01/2007","PersonnelStatID":"B","PersonnelStatDesc":"Certified and Waiting for First Match"}]
+
+
+
+GetPersStatusCodes = 
+[{"PersonnelStatID":"C","PersonnelStatDesc":"Active"},
+{"PersonnelStatID":"L","PersonnelStatDesc":"Adult Education Certification"},
+{"PersonnelStatID":"F","PersonnelStatDesc":"Assigned to Class"},
+{"PersonnelStatID":"B","PersonnelStatDesc":"Certified and Waiting for First Match"},
+{"PersonnelStatID":"P","PersonnelStatDesc":"K-12 Certification"},
+{"PersonnelStatID":"N","PersonnelStatDesc":"No Certification"},
+{"PersonnelStatID":"J","PersonnelStatDesc":"NYS Certified"}]
