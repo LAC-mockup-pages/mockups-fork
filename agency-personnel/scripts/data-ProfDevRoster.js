78d58682a7e82a31c54efd3505ef60ed8e6ffbd6
Patrick_B <pb5191@gmail.com>
2020-04-16 16:34:29 -0400

    re-arranging data sets

--- agency-personnel/scripts/data-ProfDevRoster.js
+++
@@ -1,5 +0,0 @@
-
-GetProfDevRoster =  
-[{"ID":"37193","ActivityName":"ASISTS Trainings","Provider":"provider1","Category":"Data/ASISTS","Date":"12/07/2016","Attended":"True","Hours":"3.5"},{"ID":"36288","ActivityName":"LAC FiDi Workshop","Provider":"DYCD","Category":"Program Management","Date":"02/01/2017","Attended":"True","Hours":"3"},{"ID":"38136","ActivityName":"LAC-Release 10.2 Test 6","Provider":"LAC-FIDI","Category":"NYSED/CUNY Common Core","Date":"04/01/2017","Attended":"True","Hours":"3.5"}]
-
-
