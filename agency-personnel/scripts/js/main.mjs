c8cdc511900fd78ec697a2f5d03a8eaf97496b82
Patrick_B <pb5191@gmail.com>
2020-04-19 12:44:54 -0400

    added add-new-record.mjs

--- agency-personnel/scripts/js/main.mjs
+++ agency-personnel/scripts/js/main.mjs
@@ -1,3 +1,25 @@
 //* Logic for page
 
-$(document).ready(() => {});
+$(document).ready(() => {
+  //* Back to Top button
+  const btnToTop = $("#btn-top");
+  $("window").scroll(() => {
+    btnToTop.style.display =
+      $("window").scrollTop() > 600 || $("body".scrollTop() > 600)
+        ? "inline-block"
+        : "none";
+  });
+  btnToTop.click((e) => {
+    e.stopPropagation();
+    $("html, body").animate({ scrollTop: 0 }, "600");
+  });
+
+  //* New personnel form set-up (hidden)
+  createNewRecord();
+
+  //* Adding a new team member
+  $("#add-new-member").click(function (e) {
+    e.stopPropagation();
+    $("#new-personnel").toggleClass("hidden");
+  });
+});
