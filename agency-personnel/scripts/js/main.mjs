54cf1db38a53e5c75b20227fc38059c382f49b40
Patrick_B <pb5191@gmail.com>
2020-04-21 13:36:04 -0400

    refactored saveMods

--- agency-personnel/scripts/js/main.mjs
+++ agency-personnel/scripts/js/main.mjs
@@ -1,8 +1,85 @@
 //* Logic for page
 
 import createNewRecordForm from "./add-new-record.mjs";
+import validateUserInput from "./data-check.mjs";
+import { sessionVariable } from "./data-server.mjs";
+const submittedData = [
+  {
+    name: "AgencyID",
+    value: "PRA",
+  },
+  {
+    name: "PersPersonnelID",
+    value: "",
+  },
+  {
+    name: "PersFirst",
+    value: "Paris",
+  },
+  {
+    name: "PersLast",
+    value: "Hilton",
+  },
+  {
+    name: "PersStartDate",
+    value: "1/1/2020",
+  },
+  {
+    name: "lengthstay",
+    value: "",
+  },
+  {
+    name: "PersPositionID",
+    value: "1",
+  },
+  {
+    name: "PersSubject",
+    value: "BE",
+  },
+  {
+    name: "PersPayStatus",
+    value: "V",
+  },
+  {
+    name: "PersTimeStatus",
+    value: "P",
+  },
+  {
+    name: "PersExpYears",
+    value: "2",
+  },
+];
+// Used for new personnel
+const saveMods = (form) => {
+  const result = {};
+  // const submittedData = $(form).serializeArray();
 
-// saveMods()
+  // validateUserInput() <== data-check.js
+  if (!validateUserInput(submittedData)) $(form)[0].reset();
+  for (let field of submittedData) {
+    if (field.name === "AgencyID") field.value = sessionVariable.AgencyID;
+
+    // dateFormat() <== helperFunction.js
+    if (field.name === "PersStartDate") field.value = dateFormat(field.value);
+    if (field.name === "lengthstay") {
+      const startDate = submittedData.filter(
+        (item) => item.name === "PersStartDate"
+      )[0].value;
+      field.value = moment(startDate, "MM/DD/YYYY")
+        .fromNow()
+        .replace(" ago", "");
+    }
+    result[field.name] = field.value;
+  }
+
+  //! =================================================
+  //! JSON Object to send back to database
+  console.log("result :", JSON.stringify(result));
+  //! =================================================
+
+  //ToDO Reloading/resetting with new data
+};
+
 // ======================================================
 
 $(document).ready(() => {
@@ -32,7 +109,7 @@ $(document).ready(() => {
     evnt.preventDefault();
     evnt.stopPropagation();
     const formId = "#" + $(this).attr("form");
-    // saveMods(formId)
+    saveMods(formId);
     $(formId)[0].reset();
     $(formId).toggleClass("hidden");
   });
