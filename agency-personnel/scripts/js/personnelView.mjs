b44949fdff52409be36f3bd41925f658cc899092
Patrick_B <pb5191@gmail.com>
2020-04-24 15:25:43 -0400

    started on personView

--- agency-personnel/scripts/js/personnelView.mjs
+++ agency-personnel/scripts/js/personnelView.mjs
@@ -1,3 +1,40 @@
 //* Viewing personnel info: Person, History, Professional Development
 
 import { getPersonnel } from "./data-server.mjs";
+
+const personView = (selectedID) => {
+  const personData = getPersonnel.filter((person) => person.ID === selectedID);
+  const {
+    PersFirst,
+    PersLast,
+    PersPositionID,
+    PersSubject,
+    PersStartDate,
+    lengthstay,
+    PersPayStatus,
+    PersTimeStatus,
+    PersExpYears,
+    PersHomePhone,
+    PersMobilePhone,
+    PersEmail,
+    PersAltEmail,
+  } = personData;
+
+  const labels = {
+    PersFirst: "First Name",
+    PersLast: "Last Name",
+    PersPositionID: "Position",
+    PersSubject: "Subject",
+    PersStartDate: "Start Date",
+    lengthstay: "Length of Stay (yrs)",
+    PersPayStatus: "Paid/Volunteer",
+    PersTimeStatus: "Time Status",
+    PersExpYears: "Experience",
+    PersHomePhone: "Home Phone",
+    PersMobilePhone: "Mobile Phone",
+    PersEmail: "Email",
+    PersAltEmail: "Alt Email",
+  };
+};
+
+export default personView;
