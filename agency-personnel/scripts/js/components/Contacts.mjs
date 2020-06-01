f7f92317edd06f379a805f720f52ab06d1e685b3
Patrick_B <pb5191@gmail.com>
2020-06-01 11:49:34 -0400

    added Add Form to contacts

--- agency-personnel/scripts/js/components/Contacts.mjs
+++ agency-personnel/scripts/js/components/Contacts.mjs
@@ -1,5 +1,8 @@
 import { topBanner, tableBody } from "../main.mjs";
-import { getPersProgressContacts } from "../data-server.mjs";
+import {
+  getPersProgressContacts,
+  getPersContactsCodes,
+} from "../data-server.mjs";
 
 export const contactsView = () => {
   const blockName = "Progress Contact";
@@ -15,13 +18,60 @@ export const contactsView = () => {
   recordList.forEach(
     (record) => (record.ContactDesc = record.ContactDesc.trim())
   );
-  const body = tableBody(recordList, blockName, [
-    "ID",
-    "PersonnelID",
-    "ContactType",
-  ]);
+  const body = tableBody(
+    recordList,
+    blockName,
+    ["ID", "PersonnelID", "ContactType"],
+    {
+      ContactDate: "Date",
+      ContactType: "Type",
+      ContactNotes: "Notes",
+    },
+    "PersProgressContacts"
+  );
 
   return `<div class="col-md-6">
             ${header + body}
           </div>`;
+};
+
+export const createFormAddContact = (formName) => {
+  const firstRowID = `#${formName}-0 td`;
+  const tableName = $(`#${formName}-0`).attr("data-table");
+  const firstRow = $(firstRowID).get();
+  let result = "";
+
+  for (const cell of firstRow) {
+    let optionHidden = $(cell).attr("class").includes("hidden") ? "hidden" : "";
+    let keyVal = $(cell).attr("data-field");
+    let labelVal = $(cell).attr("data-label") ? $(cell).attr("data-label") : "";
+    let option = "";
+    let value = "";
+    if (["ID", "Type"].includes(keyVal)) continue;
+    if (keyVal === "ContactType") {
+      const paramsSelect = {
+        hashTable: getPersContactsCodes,
+        keyValue: keyVal,
+        selectedValue: "",
+        labelVal,
+        labelClassVal: "",
+        option,
+      };
+      result += elementSelectWithLabel(paramsSelect);
+    } else {
+      if (keyVal === "PersonnelID") value = $(cell).text();
+
+      const paramsObj = {
+        keyVal,
+        labelVal,
+        value,
+        labelClassVal: "",
+        classVal: "style='width:10%;text-align:center'",
+        option,
+        optionHidden,
+      };
+      result += elementInput(paramsObj);
+    }
+  }
+  return [tableName, result];
 };
