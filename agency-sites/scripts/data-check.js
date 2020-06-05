0ce0993a095d9bb2a47b01d1a645df0d68abb884
Patrick_B <pb5191@gmail.com>
2020-06-05 16:06:57 -0400

    modified business rules for numbers in validateUserInput()

--- agency-sites/scripts/data-check.js
+++ agency-sites/scripts/data-check.js
@@ -1,30 +1,29 @@
 // User input data validation
 
-//* Returns true if input is only alphanumerical + underscore, not empty string
-const alphaNumCheck = (str) => {
-  return /\w+$/i.test(str);
-};
-
 const validateUserInput = (dataList) => {
+  // Returns true if input is only alphanumerical + underscore, not empty string
+  const alphaNumCheck = (str) => {
+    return /\w+$/i.test(str);
+  };
+
+  const numCheck = (str) => {
+    return /\d/g.test(str);
+  };
+
   let resultTest = true;
   for (let field of dataList) {
     switch (field.name) {
       case "ReferralSiteName":
-        resultTest = alphaNumCheck(field.value);
-        break;
       case "ReferralSiteManager":
-        resultTest = alphaNumCheck(field.value);
-        break;
       case "Address":
-        resultTest = alphaNumCheck(field.value);
-        break;
       case "City":
         resultTest = alphaNumCheck(field.value);
         break;
       case "State":
         resultTest = field.value.length < 3;
       case "Zip":
-        resultTest = field.value.length < 11;
+        resultTest = field.value.length < 11 && Number(field.value);
+        console.log("Number? >> ", Number(field.value));
         break;
       case "Telephone":
         resultTest = field.value.length < 13;
