5d8ec12a6459cec0ec19319e7cda50d5e0ee3cfb
Patrick_B <pb5191@gmail.com>
2020-06-15 17:11:15 -0400

    updated html index file to most recent

---
+++ agency-IELCE-partners/scripts/data-check.js
@@ -0,0 +1,11 @@
+//* Data check for new or edited record
+
+const validateNewRecord = (list) => {
+  // Returns true if input is only alphanumerical + underscore, not empty string
+  const alphaNumCheck = (str) => {
+    return /\w/i.test(str);
+  };
+
+  const result = [];
+  return result;
+};
