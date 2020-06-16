4eeebaf0bc33a9ac74a0dce131c4bba785570f2e
Patrick_B <pb5191@gmail.com>
2020-06-16 16:01:45 -0400

    removed test data object

--- agency-info/scripts/index.js
+++ agency-info/scripts/index.js
@@ -1,27 +1,4 @@
 //* Actions and logic
-const agTest = [
-  {
-    ID: "14",
-    AgencyID: "PRA",
-    AgencyName: "Practice Agency",
-    SEDID: "LACNYC",
-    Division: "AAA Division",
-    ProgramManager: "Ichabad Crane ",
-    Address: "85 Broad Street 27th Floor ",
-    City: "New York ",
-    State: "NY",
-    Zip: "10004",
-    Telephone: "2128033355",
-    CSD: "3",
-    EPERate: "0.0000",
-    CPD: "13",
-    CD: "5",
-    AD: "4",
-    SD: "8",
-    PrepCode: "11254",
-    AgencyEmail: "asists@gmail.com",
-  },
-];
 //! Add a <script> element in index.js pointing to data.js, then:
 let agencyData = ag[0]; //! That is all that's needed
 let newAgencyData = {};
