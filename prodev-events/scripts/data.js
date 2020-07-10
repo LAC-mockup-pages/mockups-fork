14efd4d594bf9af1bcf523a94add67b9f7ac03e4
Patrick_B <pb5191@gmail.com>
2020-07-10 11:13:39 -0400

    branch set-up

---
+++ prodev-events/scripts/data.js
@@ -0,0 +1,46 @@
+
+ProfDevEvents = [
+  {"ProfDevActivityID":"PRA12112014_4015","ID":4015,"ActNameDesc":"Asists - 12/11/14"},
+  {"ProfDevActivityID":"PRA08052017_8898","ID":8898,"ActNameDesc":"ASISTS Build 10.4  - 08/05/17"},
+  {"ProfDevActivityID":"PRA02282010_153","ID":153,"ActNameDesc":"asists Data entry Training - 02/28/10"},
+  {"ProfDevActivityID":"PRA08162016_8932","ID":8932,"ActNameDesc":"ASISTS Training 2.0 - 08/16/16"},
+  {"ProfDevActivityID":"PRA08162016_8933","ID":8933,"ActNameDesc":"ASISTS Training 2.0 - 08/16/16"}
+  ]
+  
+  ProfDevEvents2= [
+    {"ID":"4015","ProfDevActivityName":"Asists","ProfDevDescription":"morning","ProfDevDate":"12/11/2014","ProfDevProviderID":"528","ProfDevFY":"2014","ProfDevCategoryID":"53","ProfDevSubjectID":"1","ProfDevHours":"","ProfDevLocationID":"46","ProfDevFacilitator1":"1026","ProfDevFacilitator2":"2184","ProfDevFacilitator3":"2160","ProfDevFeeCharged":"","RAENEvent":"0","ProfDevComments":"","ProfDevTimeFrom":"","ProfDevTimeTo":""}
+    ]
+  
+  ProfDevRoster =[
+  {"ID":"20828","AgencyID":"PRA","PersonnelID":"cuesuzyPRA14","Name":"cue, suzy","Date":"12/11/2014","Attended":"True","FeesPaid":"False"},{"ID":"20827","AgencyID":"PRA","PersonnelID":"DainMichillePRA19","Name":"Dains, Michille","Date":"12/11/2014","Attended":"True","FeesPaid":"False"},
+  {"ID":"20831","AgencyID":"PRA","PersonnelID":"JonesTomPRA14","Name":"Jones, Tom","Date":"12/11/2014","Attended":"True","FeesPaid":"True"},{"ID":"20829","AgencyID":"PRA","PersonnelID":"Thomas # 13MarcellasPRA9","Name":"Thomas # 13, Marcellas","Date":"12/11/2014","Attended":"False","FeesPaid":"False"}
+  ]
+  
+  
+  Region = [
+  {"RAENID":"CN"},
+  {"RAENID":"CST"},
+  {"RAENID":"FL"},
+  {"RAENID":"HV"},
+  {"RAENID":"LI"},
+  {"RAENID":"NY"},
+  {"RAENID":"NYSED"},
+  {"RAENID":"WEST"}
+  ]
+  
+  
+  Agency = [
+  {"AgencyID":"PRA","AgencyName":"Practice Agency"}
+  ]
+  
+  
+  Staff = [
+  {"PersonnelID":"","Name":""},
+  {"PersonnelID":"AlfieriShelleyPRA1","Name":"Alfieri, Shelley"},
+  {"PersonnelID":"AlisovaMurianaPRA9","Name":"Alisova, Muriana"},
+  {"PersonnelID":"BarnesJuliaPRA14","Name":"Barnes, Julia"},
+  {"PersonnelID":"PorterJanePRA9","Name":"Porter, Jane"},
+  {"PersonnelID":"ScottAdamPRA9","Name":"Scott, Adam"},{"PersonnelID":"SilversRayleighPRA18","Name":"Silvers, Rayleigh"},
+  {"PersonnelID":"ZoroRoronoaPRA9","Name":"Zoro, Roronoa"}
+  ]
+  
