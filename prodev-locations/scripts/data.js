68d999ffa91f448e5e527efe87eff7331f72fe6e
Patrick_B <pb5191@gmail.com>
2020-07-10 10:44:48 -0400

    branch set up

---
+++ prodev-locations/scripts/data.js
@@ -0,0 +1,18 @@
+
+Locations =  [
+  {"ID":"1","FacilityName":"loco1","Address":"","City":"","State":"NY","Zip":"","Phone":"(___) ___-____","Email":""},{"ID":"46","FacilityName":"Utica","Address":"309 Genessee Street","City":"Utica","State":"NY","Zip":"","Phone":"","Email":""},{"ID":"812","FacilityName":"DYCD","Address":"","City":"","State":"NY","Zip":"","Phone":"","Email":""},{"ID":"1534","FacilityName":"LAC-FiDi","Address":"89 Broad Street ","City":"New York City ","State":"NY","Zip":"10003","Phone":"2125553699","Email":""},{"ID":"1591","FacilityName":"Literacy Assistance Center","Address":"85 Broad Street ","City":"New York ","State":"NY","Zip":"10004","Phone":"2125553355","Email":""}
+  ]
+  
+  
+  
+  States = [
+  {"Abbrev":"AK","State":"Alaska"},
+  {"Abbrev":"FL","State":"Florida"},
+  {"Abbrev":"MA","State":"Massachussetts"},
+  {"Abbrev":"MD","State":"Maryland"},
+  {"Abbrev":"NJ","State":"New Jersey"},
+  {"Abbrev":"NY","State":"New York"},
+  {"Abbrev":"PA","State":"Pennsylvania"},
+  {"Abbrev":"PR","State":"Puerto Rico"},
+  {"Abbrev":"WY","State":"Wyoming"}
+  ]
