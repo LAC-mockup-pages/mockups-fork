429ba19df507677ce36c8935d3ba0e16d3be7476
Patrick_B <pb5191@gmail.com>
2020-07-10 11:06:48 -0400

    added data objects

---
+++ prodev-facilitators/scripts/data.js
@@ -0,0 +1,18 @@
+
+Facilitator =  [
+  {"ID":"1","FacFirstName":"test1","FacLastName":"new","Address":"","City":"","State":"NY","Zip":"","HomePhone":"2125555555","Email":"qqwwqq","CellPhone":"","AlternatePhone":""},{"ID":"1026","FacFirstName":"Vutha","FacLastName":"Nguy","Address":"","City":"","State":"NY","Zip":"","HomePhone":"1111111111","Email":"vnguy@dycd.com","CellPhone":"","AlternatePhone":""},{"ID":"2184","FacFirstName":"Iglesias","FacLastName":"Lizelena","Address":"89 Broad Street","City":"New York","State":"NY","Zip":"10009","HomePhone":"2125556987","Email":"","CellPhone":"3475555896","AlternatePhone":""},{"ID":"2305","FacFirstName":"Srikanth","FacLastName":"Srinivasan","Address":"85 Broad Street ","City":"New York ","State":"NY","Zip":"11246","HomePhone":"2125554692","Email":"pra@asists.com","CellPhone":"","AlternatePhone":""},{"ID":"2160","FacFirstName":"Raju","FacLastName":"Srinivasan","Address":"455 5th Avenue ","City":"New York City ","State":"NY","Zip":"10016","HomePhone":"2125555555","Email":"rajus@lacnyc.org","CellPhone":"","AlternatePhone":""}
+  ]
+  
+  
+  
+  States = [
+  {"Abbrev":"AK","State":"Alaska"},
+  {"Abbrev":"FL","State":"Florida"},
+  {"Abbrev":"MA","State":"Massachussetts"},
+  {"Abbrev":"MD","State":"Maryland"},
+  {"Abbrev":"NJ","State":"New Jersey"},
+  {"Abbrev":"NY","State":"New York"},
+  {"Abbrev":"PA","State":"Pennsylvania"},
+  {"Abbrev":"PR","State":"Puerto Rico"},
+  {"Abbrev":"WY","State":"Wyoming"}
+  ]
