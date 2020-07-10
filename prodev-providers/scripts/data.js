8f5cb66400ad94ef7ce8ebfc4c2ce7b55e4d9643
Patrick_B <pb5191@gmail.com>
2020-07-10 11:00:35 -0400

    branch set-up

---
+++ prodev-providers/scripts/data.js
@@ -0,0 +1,18 @@
+
+Providers =  [
+  {"ID":"528","ProviderName":"DYCD","Address":"","City":"","State":"NY","Zip":"","Phone":"0000000000","Email":"000"},{"ID":"959","ProviderName":"LAC-FIDI","Address":"89 Broad Street","City":"New York","State":"NY","Zip":"10009","Phone":"2125558035","Email":""},{"ID":"992","ProviderName":"Literacy Assistance Center ","Address":"85 Broad Street ","City":"New York ","State":"NY","Zip":"10004","Phone":"2125553355","Email":"pra@asists.com"},{"ID":"1","ProviderName":"provider1","Address":"","City":"","State":"NY","Zip":"","Phone":"2125555555","Email":"ggffghgfh"}
+  ]
+  
+  
+  
+  States = [
+  {"Abbrev":"AK","State":"Alaska"},
+  {"Abbrev":"FL","State":"Florida"},
+  {"Abbrev":"MA","State":"Massachussetts"},
+  {"Abbrev":"MD","State":"Maryland"},
+  {"Abbrev":"NJ","State":"New Jersey"},
+  {"Abbrev":"NY","State":"New York"},
+  {"Abbrev":"PA","State":"Pennsylvania"},
+  {"Abbrev":"PR","State":"Puerto Rico"},
+  {"Abbrev":"WY","State":"Wyoming"}
+  ]
