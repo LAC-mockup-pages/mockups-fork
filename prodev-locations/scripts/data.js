e6f13ce68862b0e8a1e81ab18e141cc814723e7d
Patrick_B <pb5191@gmail.com>
2020-07-13 08:25:12 -0400

    added phone and emails where missing

--- prodev-locations/scripts/data.js
+++ prodev-locations/scripts/data.js
@@ -1,6 +1,6 @@
 
 const Locations =  [
-  {"ID":"1","FacilityName":"loco1","Address":"","City":"","State":"NY","Zip":"","Phone":"(___) ___-____","Email":""},{"ID":"46","FacilityName":"Utica","Address":"309 Genessee Street","City":"Utica","State":"NY","Zip":"","Phone":"","Email":""},{"ID":"812","FacilityName":"DYCD","Address":"","City":"","State":"NY","Zip":"","Phone":"","Email":""},{"ID":"1534","FacilityName":"LAC-FiDi","Address":"89 Broad Street ","City":"New York City ","State":"NY","Zip":"10003","Phone":"2125553699","Email":""},{"ID":"1591","FacilityName":"Literacy Assistance Center","Address":"85 Broad Street ","City":"New York ","State":"NY","Zip":"10004","Phone":"2125553355","Email":""}
+  {"ID":"1","FacilityName":"loco1","Address":"","City":"","State":"NY","Zip":"","Phone":"1231231234","Email":"email@email.com"},{"ID":"46","FacilityName":"Utica","Address":"309 Genessee Street","City":"Utica","State":"NY","Zip":"","Phone":"212-316-3167","Email":"email@utica.org"},{"ID":"812","FacilityName":"DYCD","Address":"","City":"","State":"NY","Zip":"","Phone":"6465556677","Email":"john-smith@dycd.org"},{"ID":"1534","FacilityName":"LAC-FiDi","Address":"89 Broad Street ","City":"New York City ","State":"NY","Zip":"10003","Phone":"2125553699","Email":"manager@lac-fidi.org"},{"ID":"1591","FacilityName":"Literacy Assistance Center","Address":"85 Broad Street ","City":"New York ","State":"NY","Zip":"10004","Phone":"2125553355","Email":"alamo@litcenter.com"}
   ]
   
   const sessionVariable = {
