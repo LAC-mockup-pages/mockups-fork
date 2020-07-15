0307e7e3b45383063945091d276c026582fc2102
Patrick_B <pb5191@gmail.com>
2020-07-15 16:36:06 -0400

    added data objects for categories and subjects

--- prodev-events/scripts/data.js
+++ prodev-events/scripts/data.js
@@ -88,6 +88,27 @@ const Staff = [
 //   ]
   
 
+const Categories = [
+  {"CATEGORYID":50,"Category":"ABE/HSE"},
+  {"CATEGORYID":1,"Category":"Assessment"},
+  {"CATEGORYID":53,"Category":"Common Core Content Related"},
+  {"CATEGORYID":52,"Category":"Common Core Instructional Practice"},
+  {"CATEGORYID":26,"Category":"Data/ASISTS"},
+  {"CATEGORYID":55,"Category":"Distance Learning"},
+  {"CATEGORYID":51,"Category":"ESOL"}
+  ]
+  
+  
+const  Subjects = [
+  {"SubjectID":1,"SubjectDesc":"ELA"},
+  {"SubjectID":2,"SubjectDesc":"Social Studies"},
+  {"SubjectID":3,"SubjectDesc":"Science"},
+  {"SubjectID":4,"SubjectDesc":"Math"},
+  {"SubjectID":5,"SubjectDesc":"Career Pathways"},
+  {"SubjectID":6,"SubjectDesc":"ESL"}
+  ]
+  
+
 const Facilitator =  [
   {"ID":"1","FacFirstName":"test1","FacLastName":"new","Address":"","City":"","State":"NY","Zip":"","HomePhone":"2125555555","Email":"qqwwqq","CellPhone":"","AlternatePhone":""},{"ID":"1026","FacFirstName":"Vutha","FacLastName":"Nguy","Address":"","City":"","State":"NY","Zip":"","HomePhone":"1111111111","Email":"vnguy@dycd.com","CellPhone":"","AlternatePhone":""},{"ID":"2184","FacFirstName":"Iglesias","FacLastName":"Lizelena","Address":"89 Broad Street","City":"New York","State":"NY","Zip":"10009","HomePhone":"2125556987","Email":"","CellPhone":"3475555896","AlternatePhone":""},{"ID":"2305","FacFirstName":"Srikanth","FacLastName":"Srinivasan","Address":"85 Broad Street ","City":"New York ","State":"NY","Zip":"11246","HomePhone":"2125554692","Email":"pra@asists.com","CellPhone":"","AlternatePhone":""},{"ID":"2160","FacFirstName":"Raju","FacLastName":"Srinivasan","Address":"455 5th Avenue ","City":"New York City ","State":"NY","Zip":"10016","HomePhone":"2125555555","Email":"rajus@lacnyc.org","CellPhone":"","AlternatePhone":""}
   ]
