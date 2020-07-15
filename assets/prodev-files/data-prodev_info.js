16269f1594e226fbfa144c805992fd2c9de24d1a
Patrick_B <pb5191@gmail.com>
2020-07-15 10:36:43 -0400

    added files for page

---
+++ assets/prodev-files/data-prodev_info.js
@@ -0,0 +1,60 @@
+
+ProfDevEvent = [
+{"ID":"4015","ProfDevActivityName":"Asists","ProfDevDescription":"morning","ProfDevDate":"12/11/2014","ProfDevProviderID":"528","ProfDevFY":"2014","ProfDevCategoryID":"53","ProfDevSubjectID":"1","ProfDevHours":"","ProfDevLocationID":"46","ProfDevFacilitator1":"1026","ProfDevFacilitator2":"2184","ProfDevFacilitator3":"2160","ProfDevFeeCharged":"","RAENEvent":"0","ProfDevComments":"","ProfDevTimeFrom":"","ProfDevTimeTo":""}
+]
+
+
+FiscalYear = [
+{"FiscalYear":"2020"},
+{"FiscalYear":"2019"},
+{"FiscalYear":"2018"},
+{"FiscalYear":"2017"}
+]
+
+
+Providers = [
+{"ProviderID":528,"ProviderName":"DYCD"},
+{"ProviderID":959,"ProviderName":"LAC-FIDI"},
+{"ProviderID":992,"ProviderName":"Literacy Assistance Center"},
+{"ProviderID":1,"ProviderName":"provider1"}
+]
+
+
+Categories = [
+{"CATEGORYID":50,"Category":"ABE/HSE"},
+{"CATEGORYID":1,"Category":"Assessment"},
+{"CATEGORYID":53,"Category":"Common Core Content Related"},
+{"CATEGORYID":52,"Category":"Common Core Instructional Practice"},
+{"CATEGORYID":26,"Category":"Data/ASISTS"},
+{"CATEGORYID":55,"Category":"Distance Learning"},
+{"CATEGORYID":51,"Category":"ESOL"}
+]
+
+
+Subjects = [
+{"SubjectID":1,"SubjectDesc":"ELA"},
+{"SubjectID":2,"SubjectDesc":"Social Studies"},
+{"SubjectID":3,"SubjectDesc":"Science"},
+{"SubjectID":4,"SubjectDesc":"Math"},
+{"SubjectID":5,"SubjectDesc":"Career Pathways"},
+{"SubjectID":6,"SubjectDesc":"ESL"}
+]
+
+
+Facilitators = [
+{"FacilitatorID":2184,"AgencyID":"PRA","FacLastName":"Lizelena","FacFirstName":"Iglesias","FullName":"Lizelena Iglesias"},
+{"FacilitatorID":1,"AgencyID":"PRA","FacLastName":"new","FacFirstName":"test1","FullName":"new test1"},
+{"FacilitatorID":1026,"AgencyID":"PRA","FacLastName":"Nguy","FacFirstName":"Vutha","FullName":"Nguy Vutha"},
+{"FacilitatorID":2160,"AgencyID":"PRA","FacLastName":"Srinivasan","FacFirstName":"Raju","FullName":"Srinivasan Raju"},
+{"FacilitatorID":2305,"AgencyID":"PRA","FacLastName":"Srinivasan","FacFirstName":"Srikanth","FullName":"Srinivasan Srikanth"}
+]
+
+
+StaffLocations = [
+{"LocationId":812,"FacilityName":"DYCD"},
+{"LocationId":1534,"FacilityName":"LAC-FiDi"},
+{"LocationId":1591,"FacilityName":"Literacy Assistance Center"},
+{"LocationId":1,"FacilityName":"loco1"},
+{"LocationId":46,"FacilityName":"Utica"}
+]
+
