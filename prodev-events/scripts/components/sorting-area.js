54ea3b08da179e7864f5b74acb87d7376061fdc8
Patrick_B <pb5191@gmail.com>
2020-08-05 16:35:33 -0400

    setting up the block

---
+++ prodev-events/scripts/components/sorting-area.js
@@ -0,0 +1,22 @@
+// Block for sorting events in main-table
+const createSortZone = () => {
+  let bloc = "";
+  const selectList = [
+    {
+      ProfDevActivityName: "Name",
+      ProfDevDate: "Date",
+      ProfDevDescription: "Session",
+      ProfDevProviderID: "Provider",
+      ProfDevLocationID: "Location",
+      ProfDevCategoryID: "Category",
+      ProfDevSubjectID: "Subject",
+      ProfDevFacilitator1: "Facilitator1",
+      profdevFac1: "Facilitator1",
+      ProfDevFacilitator2: "Facilitator2",
+      profdevFac2: "Facilitator2",
+      ProfDevFacilitator3: "Facilitator3",
+      profdevFac3: "Facilitator3"
+    }
+  ];
+  return bloc;
+};
