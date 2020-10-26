a9f7ad1c15e48e16a9facbf1f143dc1fb304f64e
Patrick_B <pb5191@gmail.com>
2020-10-26 10:57:53 -0400

    added instructor table and form

--- instructional-activity/scripts/components/Instructors.js
+++ instructional-activity/scripts/components/Instructors.js
@@ -1,25 +1,49 @@
 // Bloc for main instructor and additional intructors
 
-const instructorList = GetInstructor.slice(0).sort((name1, name2) =>
-  name1.InstructorName < name2.InstructorName
-    ? -1
-    : name1.InstructorName > name2.InstructorName
-    ? 1
-    : 0
-);
-
-const classInstructorList = GetClassInstructor.slice(0).sort((name1, name2) =>
-  name1.Name < name2.Name ? -1 : name1.Name > name2.Name ? 1 : 0
-);
+import { topBanner } from "../main.js";
 
 const instructorsBloc = () => {
-  return "<div class='col-md-6'><h3>Instructors Bloc</h3></div>";
-};
+  let bloc = topBanner("Instructors");
+  const instructorList = GetInstructor.slice(0).sort((name1, name2) =>
+    name1.InstructorName < name2.InstructorName
+      ? -1
+      : name1.InstructorName > name2.InstructorName
+      ? 1
+      : 0
+  );
 
-const additionalInfoBloc = () => {
-  return "<div class='col-md-6'><h3>All the other info</h3></div> ";
+  const classInstructorList = GetClassInstructor.slice(0).sort((name1, name2) =>
+    name1.Name < name2.Name ? -1 : name1.Name > name2.Name ? 1 : 0
+  );
+  let instructorView = "";
+
+  for (let i = 0; i < 3; i++) {
+    const instructor = classInstructorList[i] ? classInstructorList[i] : "";
+    let instructorRow = "";
+
+    if (instructor) {
+    } else {
+      instructorRow = "<tr><td></td><td>Instructor Selection</td></tr>";
+    }
+  }
+
+  const tableInstructors = `<form class="instructors" id="instructors-form">
+  <table class="instructors-table container-fluid table table-bordered"
+  <thead>
+   <th>Date Assigned</th>
+   <th>Instructors</th>
+   </thead>
+   <tbody>
+   ${instructorView}
+   </tbody>
+   </table>
+   </form>`;
+
+  bloc += tableInstructors;
+
+  return bloc;
 };
 
 export const createAdditionalBloc = () => {
-  return `${instructorsBloc()}${additionalInfoBloc()}`;
+  return `${instructorsBloc()}`;
 };
