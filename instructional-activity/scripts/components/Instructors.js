4f0149af116ac4221227b73f64c9a2c0f4b3dc0c
Patrick_B <pb5191@gmail.com>
2020-10-26 16:53:11 -0400

    added instructor options and select

--- instructional-activity/scripts/components/Instructors.js
+++ instructional-activity/scripts/components/Instructors.js
@@ -19,16 +19,29 @@ const instructorsBloc = () => {
 
   for (let i = 0; i < 3; i++) {
     const instructor = classInstructorList[i] ? classInstructorList[i] : "";
+
     let instructorRow = "";
 
     if (instructor) {
+      const { ID, PersonnelID, AssignDate, Name } = instructor;
+      const dateInput = inputNoLabel({
+        key,
+        placehold,
+        classOption,
+        option,
+        type
+      });
+      const instructorOption = "";
+      const instructorNameSelect = `<select class="instructor-name"`;
+
+      instructorRow = `<tr><td><input class="assign-date" type="date" name="AssignDate" value=${AssignDate}/></td><td>< class="instructor-name" type="text" name=${Name}</td></tr>`;
     } else {
       instructorRow = "<tr><td></td><td>Instructor Selection</td></tr>";
     }
   }
 
-  const tableInstructors = `<form class="instructors" id="instructors-form">
-  <table class="instructors-table container-fluid table table-bordered"
+  const tableInstructors = `<form class="instructors col-md-6" id="instructors-form">
+  <table class="instructors-table table table-bordered"
   <thead>
    <th>Date Assigned</th>
    <th>Instructors</th>
