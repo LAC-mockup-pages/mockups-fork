998c7ee3fcaf99a448c74aca061f60545633b6c4
Patrick_B <pb5191@gmail.com>
2020-10-29 11:34:46 -0400

    modified schedule bloc sub header

--- instructional-activity/scripts/components/Schedule.js
+++ instructional-activity/scripts/components/Schedule.js
@@ -6,9 +6,37 @@
 
 import { topBanner } from "../main.js";
 
+const classDays = (dataObj) => {
+  const weekDays = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ];
+  const filteredList = Object.keys(dataObj)
+    .filter((key) => weekDays.includes(key))
+    .map((day) => {
+      const classDayObj = {};
+      const classStart = `${day.slice(0, 3)}StartTime`;
+      const classEnd = `${day.slice(0, 3)}EndTime`;
+
+      classDayObj[day] = dataObj[day];
+      classDayObj[classStart] = dataObj[classStart];
+      classDayObj[classEnd] = dataObj[classEnd];
+      return classDayObj;
+    });
+  return filteredList;
+};
+
 export const createSchedule = (dataObj) => {
+  console.log("datObj Schedule :>> ", classDays(dataObj));
+  const weekDaysList = classDays(dataObj);
+
   let bloc = topBanner("Schedule");
-  const weekDaysBanner = `
+  const weekDaysHeader = `
   <div class="schedule-bloc">
   <form class="weekday-banner" id="week-schedule">
  <table class="schedule-table container-fluid table table-bordered"
@@ -21,33 +49,34 @@ export const createSchedule = (dataObj) => {
   <th class="weekday">Fri. </th>
   <th class="weekday">Sat.</th>
   <th class="weekday">Sun.</th>
-
  </thead>
- <tbody class="schedule-body">
- <tr>
- <td class="schedule-cell">Start Time</td>
- <td><input class="schedule-input" value="09:30 AM"/></td>
- <td><input class="schedule-input" value="09:30 AM"/></td>
- <td><input class="schedule-input" value="09:30 AM"/></td>
- <td><input class="schedule-input" value="09:30 AM"/></td>
- <td><input class="schedule-input" value="09:30 AM"/></td>
- <td><input class="schedule-input" value="09:30 AM"/></td>
- <td><input class="schedule-input" value="09:30 AM"/></td>
-</tr>
-<tr>
-<td class="schedule-cell">End Time</td>
-<td><input class="schedule-input" value="11:30 AM"/></td>
-<td><input class="schedule-input" value="11:30 AM"/></td>
-<td><input class="schedule-input" value="11:30 AM"/></td>
-<td><input class="schedule-input" value="11:30 AM"/></td>
-<td><input class="schedule-input" value="11:30 AM"/></td>
-<td><input class="schedule-input" value="11:30 AM"/></td>
-<td><input class="schedule-input" value="11:30 AM"/></td>
-</tr>
- </tbody>
-</table>
- </form>
- </div>`;
-  bloc += weekDaysBanner;
+ </table></form></div>`;
+
+  //  <tbody class="schedule-body">
+  //  <tr>
+  //  <td class="schedule-cell">Start Time</td>
+  //  <td><input class="schedule-input" value="09:30 AM"/></td>
+  //  <td><input class="schedule-input" value="09:30 AM"/></td>
+  //  <td><input class="schedule-input" value="09:30 AM"/></td>
+  //  <td><input class="schedule-input" value="09:30 AM"/></td>
+  //  <td><input class="schedule-input" value="09:30 AM"/></td>
+  //  <td><input class="schedule-input" value="09:30 AM"/></td>
+  //  <td><input class="schedule-input" value="09:30 AM"/></td>
+  // </tr>
+  // <tr>
+  // <td class="schedule-cell">End Time</td>
+  // <td><input class="schedule-input" value="11:30 AM"/></td>
+  // <td><input class="schedule-input" value="11:30 AM"/></td>
+  // <td><input class="schedule-input" value="11:30 AM"/></td>
+  // <td><input class="schedule-input" value="11:30 AM"/></td>
+  // <td><input class="schedule-input" value="11:30 AM"/></td>
+  // <td><input class="schedule-input" value="11:30 AM"/></td>
+  // <td><input class="schedule-input" value="11:30 AM"/></td>
+  // </tr>
+  //  </tbody>
+  // </table>
+  //  </form>
+  //  </div>`;
+  bloc += weekDaysHeader;
   return bloc;
 };
