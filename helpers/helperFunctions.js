6ac7a44c47a0b0f9206e636c25c7d4b2e82260ff
Patrick_B <pb5191@gmail.com>
2020-11-02 08:50:32 -0500

    added date validation to helper functions

--- helpers/helperFunctions.js
+++ helpers/helperFunctions.js
@@ -227,3 +227,28 @@ const createRow = (argsObj) => {
     ""
   )}</tr>`;
 };
+
+// Date validation for MM/DD/YYYY.
+// input is a string, output is Boolean
+const dateValid = (date) => {
+  const isLeapYear = (yearNum) => {
+    return (yearNum % 100 === 0 ? yearNum % 400 === 0 : yearNum % 4 === 0)
+      ? 1
+      : 0;
+  };
+  const match = date.match(/^(\d\d)\/(\d\d)\/(\d{4})$/) || [];
+  const month = (match[1] | 0) - 1;
+  const day = match[2] | 0;
+  const year = match[3] | 0;
+
+  const dateEval = !(
+    month < 0 || // Before January
+    month > 11 || // After December
+    day < 1 || // Before the 1st of the month
+    day - 30 > ((2773 >> month) & 1) || // Day is 30 or 31 using bitmap.
+    // Day is 28 or 29, month is 02, year is leap year ==> true
+    (month === 1 && day - 28 > isLeapYear(year))
+  );
+
+  return dateEval;
+};
