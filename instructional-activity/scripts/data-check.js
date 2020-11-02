b04579cbb5d7d25ad731b3b2f65b9183444168b7
Patrick_B <pb5191@gmail.com>
2020-11-02 10:17:21 -0500

    modified date validatin

--- instructional-activity/scripts/data-check.js
+++ instructional-activity/scripts/data-check.js
@@ -5,8 +5,6 @@ const validateRecord = (dataList, requiredList) => {
   const alphaNumCheck = (str) => {
     return !/[^\s\w-.,]/g.test(str);
   };
-  // Regex for date Format as MM/DD/YYYY
-  const dateFormat = /^(0?[1-9]|1[012])[\/](0?[1-9]|[12][0-9]|3[01])[\/]\d{4}$/;
 
   const resultList = [];
   for (let field of dataList) {
@@ -16,8 +14,7 @@ const validateRecord = (dataList, requiredList) => {
     switch (name) {
       case "StartDate":
       case "EndDate":
-        // Match the date format through regular expression
-        obj.correct = value.match(dateFormat) ? true : false;
+        obj.correct = dateValid(value);
         break;
 
       default:
