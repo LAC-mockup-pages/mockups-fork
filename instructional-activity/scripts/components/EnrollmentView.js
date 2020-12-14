b098ba659a33bd40d918bf144b303ce88be19513
Patrick_B <pb5191@gmail.com>
2020-12-14 17:13:45 -0500

    added headers for student table

--- instructional-activity/scripts/components/EnrollmentView.js
+++ instructional-activity/scripts/components/EnrollmentView.js
@@ -1,6 +1,34 @@
 // Displays student enrollment for a course
 // and instructors hours
 
+const createStudentBloc = (dataList) => {
+  const labelsStudentObj = {
+    ID: "ID",
+    Student_PKID: "StudentPKID",
+    StudentID: "Student ID",
+    StudentName: "Name",
+    EnrollDate: "Start Date",
+    ActiveStatus: "Status",
+    InactiveDate: "End Date",
+    InactiveReason: "Reason",
+    TransferTo: "Transfer To"
+  };
+
+  const list = Object.keys(labelsStudentObj).map(
+    (item) => labelsStudentObj[item]
+  );
+
+  console.log("list :>> ", list);
+
+  // createHeaders() <== helperFunctions.js
+  const studentTableHeader = createHeaders(list);
+  const studentTableBody = "<h3>Table body</h3>";
+
+  return `
+  <div>${studentTableHeader}</div>
+  <div>${studentTableBody}</div>`;
+};
+
 export const createEnrollmentView = (courseID, courseName) => {
   const mainBanner = `
   <div class="blue-bg container-fluid row" id="main-banner">
@@ -9,8 +37,11 @@ export const createEnrollmentView = (courseID, courseName) => {
     <button type="button" id="add-student-btn" class="btn dark-blue-text blue-light-bg">Add Student</button></div>
   </div>`;
 
+  const studentTable = createStudentBloc(GetEnrollInfo);
+
   return `
-  <div class="container-fluid" id="enrollment-bloc" data-course-id=${courseID}>
+  < class="container-fluid" id="enrollment-bloc" data-course-id=${courseID}>
     ${mainBanner}
+    ${studentTable}
   </div>`;
 };
