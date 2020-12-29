7d85f66ee2eef596a9af78573bdd5ddeb1ccdb2d
Patrick_B <pb5191@gmail.com>
2020-12-29 10:10:11 -0500

    added README and color table

--- dashboard-landing/scripts/main.js
+++ dashboard-landing/scripts/main.js
@@ -1,2 +1,28 @@
-// Action and logic for the page.
-// JS and jQuery
+//*=================================================
+//* Action and logic for the page.
+//* JS and jQuery
+//*=================================================
+
+const tileSet = [
+  { id: "tile0", header: "MSG", background: "", details: "", format: "" },
+  { id: "tile1", header: "MSG", background: "", details: "", format: "" },
+  { id: "tile2", header: "MSG", background: "", details: "", format: "" },
+  { id: "tile3", header: "MSG", background: "", details: "", format: "" },
+  { id: "tile4", header: "MSG", background: "", details: "", format: "" },
+  { id: "tile5", header: "MSG", background: "", details: "", format: "" },
+  { id: "tile6", header: "MSG", background: "", details: "", format: "" },
+  { id: "tile7", header: "MSG", background: "", details: "", format: "" },
+  { id: "tile8", header: "MSG", background: "", details: "", format: "" },
+  { id: "tile9", header: "MSG", background: "", details: "", format: "" }
+];
+
+const createBlock = () => {
+  let block = "";
+
+  return block;
+};
+//*=================================================
+//* jQuery section
+//*=================================================
+
+$(document).ready(() => {});
