5a6233343f60de928e8f9f3865c2d77a01a5e659
Patrick_B <pb5191@gmail.com>
2021-01-05 12:58:13 -0500

    adding imports

--- dashboard-msg/scripts/main.js
+++ dashboard-msg/scripts/main.js
@@ -1,1 +1,22 @@
-// Logic for dashboard
+//*=================================================
+//* Action and logic for the page.
+//* JS and jQuery
+//*=================================================
+
+import {
+  tileSet,
+  createDetailLines,
+  createTile
+} from "../../dashboard-landing/scripts/main.js";
+
+const createLeftNavBar = () => {};
+
+//*=================================================
+//* jQuery section
+//*=================================================
+
+$(document).ready(() => {
+  const leftNavBar = createLeftNavBar();
+
+  $("#side-nav").append(leftNavBar);
+});
