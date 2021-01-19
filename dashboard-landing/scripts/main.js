12bdcd770f152d734fb34214ce8b76885e0a86b2
Patrick_B <pb5191@gmail.com>
2021-01-19 09:48:33 -0500

    removed commented out code

--- dashboard-landing/scripts/main.js
+++ dashboard-landing/scripts/main.js
@@ -3,89 +3,6 @@
 //* JS and jQuery
 //*=================================================
 
-// export const tileSet = [
-//   {
-//     id: "tile0",
-//     header: "MSG",
-//     background: "rgba(36,121,181,0), rgba(36,121,181,1)",
-//     details: ["64%"],
-//     formatDetails: "",
-//     target: "../dashboard-msg/index.html"
-//   },
-//   {
-//     id: "tile1",
-//     header: "Post Test",
-//     background: "rgba(240,226,213,0), rgba(240,226,213,1)",
-//     details: ["75%"],
-//     formatDetails: "",
-//     target: "../dashboard-msg/index.html"
-//   },
-//   {
-//     id: "tile2",
-//     header: "Employment Q2",
-//     background: "rgba(172,143,194,0), rgba(172,143,194,1)",
-//     details: ["52%"],
-//     formatDetails: "",
-//     target: "../assets/coming-soon.html"
-//   },
-//   {
-//     id: "tile3",
-//     header: "Employment Q4",
-//     background: "rgba(166,166,166,0), rgba(166,166,166,1)",
-//     details: ["23%"],
-//     formatDetails: "",
-//     target: "../assets/coming-soon.html"
-//   },
-//   {
-//     id: "tile4",
-//     header: "EPE",
-//     background: "rgba(155,187,202,0), rgba(155,187,202,1)",
-//     details: ["Enrolled: 450", "Hours: 22,000"],
-//     formatDetails: "tile-details-small",
-//     target: "../assets/coming-soon.html"
-//   },
-//   {
-//     id: "tile5",
-//     header: "Enrolled",
-//     background: "rgba(36,121,181,0), rgba(36,121,181,1)",
-//     details: ["502"],
-//     formatDetails: "",
-//     target: "../assets/coming-soon.html"
-//   },
-//   {
-//     id: "tile6",
-//     header: "HSE",
-//     background: "rgba(247,224,149,0), rgba(247,224,149,1)",
-//     details: ["Referred: 37", "Passed: 28"],
-//     formatDetails: "tile-details-small",
-//     target: "../assets/coming-soon.html"
-//   },
-//   {
-//     id: "tile7",
-//     header: "TABE Tests",
-//     background: "rgba(172,143,194,0), rgba(172,143,194,1)",
-//     details: ["Pre: 507", "Post: 205"],
-//     formatDetails: "tile-details-small",
-//     target: "../assets/coming-soon.html"
-//   },
-//   {
-//     id: "tile8",
-//     header: "Best + Tests",
-//     background: "rgba(166,166,166,0), rgba(166,166,166,1)",
-//     details: ["Pre: 300", "Post: 180"],
-//     formatDetails: "tile-details-small",
-//     target: "../assets/coming-soon.html"
-//   },
-//   {
-//     id: "tile9",
-//     header: "Hours",
-//     background: "rgba(240,226,213,0), rgba(240,226,213,1)",
-//     details: ["15,000"],
-//     formatDetails: "",
-//     target: "../assets/coming-soon.html"
-//   }
-// ];
-
 export const tileSet = [
   {
     id: "tile0",
@@ -182,7 +99,6 @@ export const tileSet = [
 ];
 
 export const createDetailLines = (detailList) => {
-  // return detailList.map((item) => `<div>${item}</div>`).join("");
   let rows = "";
   for (const list of detailList) {
     rows += `<tr><td>${list[0]}</td><td>${list[1]}</td></tr>`;
@@ -217,21 +133,6 @@ export const createTile = (dataObj, classButton, classTile) => {
   return tile;
 };
 
-//* Creates bloc of same-size tiles with 5 tiles per bloc.
-//* Suitable for 2 rows of tiles.
-// const createBlock = (tileBloc) => {
-//   let block = "";
-//   for (const record of tileBloc) {
-//     block += createTile(record, "col-md-2");
-//   }
-//   return `
-//   <div class="container-fluid row">
-//     <div class="col-md-1"></div>
-//     ${block}
-//     <div class="col-md-1"></div>
-//   </div>`;
-// };
-
 //* Creates bloc of large tiles, 2 tiles per bloc.
 const createLargeTileBloc = (tileList) => {
   let block = "";
@@ -263,9 +164,6 @@ const createSmallTileBloc = (tileList) => {
 //*=================================================
 
 $(document).ready(() => {
-  // const topBloc = createBlock(tileSet.slice(0, 5));
-  // const bottomBloc = createBlock(tileSet.slice(5));
-
   const topBloc = createLargeTileBloc(tileSet.slice(0, 2));
   const middleBloc = createLargeTileBloc(tileSet.slice(2, 4));
   const bottomBloc = createSmallTileBloc(tileSet.slice(4));
