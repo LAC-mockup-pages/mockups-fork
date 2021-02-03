f334fb51d089b971286db92f28015d1392bfa293
Patrick_B <pb5191@gmail.com>
2021-02-03 10:12:05 -0500

    removed jQuery separate file

--- instructional-activity/scripts/main.js
+++ instructional-activity/scripts/main.js
@@ -571,6 +571,34 @@ $(document).ready(() => {
     $("#edit-form").attr("data-bloc", formId);
   });
 
+  //* Displaying selected record in Enrollment View.
+  $(document).on("click", ".main-table tr", function (evnt) {
+    evnt.stopPropagation();
+    const rowId = $(this).attr("id");
+    const selectedCourse = courseList.find((course) => course.ID === rowId);
+    const { ClassID, StartDate, EndDate } = selectedCourse;
+
+    const enrollmentView = createEnrollmentView(
+      rowId,
+      ClassID,
+      StartDate,
+      EndDate
+    );
+
+    // Cleaning up
+    $(".record-entry, #filter-bloc").toggleClass("hidden");
+    $("#view-bloc").empty().append(enrollmentView).attr("data-course", rowId);
+
+    $("html, body").animate({ scrollTop: 220 }, 200);
+    $("#offering").removeClass();
+    $("#enrollment").addClass("selected-tab");
+    $("#sub-nav .unselected-tab").removeClass("unselected-tab");
+    $("#sub-nav .btn-link").prop("disabled", false).removeClass("disabled");
+
+    // Enables customized tooltips
+    $("[data-toggle='tooltip']").tooltip();
+  });
+
   //* Saving schedule/funding sources
   $(document).on("click", ".save-record-btn", function (evnt) {
     evnt.stopPropagation();
@@ -584,5 +612,51 @@ $(document).ready(() => {
     console.log("savedData :>> ", savedData);
 
     saveMods(savedData, formId, "GetCourse");
+  });
+
+  //* Adding a new student in Enrollment view
+  $(document).on("click", "#add-student-btn", function () {
+    const classID = $("#main-banner .label-text").text();
+    const coursePKId = $("#view-bloc").data("course");
+    const courseStartDate = $(this).data("start");
+    const modalContent = addStudentModalForm(
+      coursePKId,
+      classID,
+      courseStartDate
+    );
+    $("#modalBloc").modal("toggle");
+    $("#edit-form")
+      .empty()
+      .append(modalContent)
+      .attr("data-bloc", "add-student");
+    $(".modal-title").replaceWith(
+      "<h4 class='modal-title'>Enrolling a student</h4>"
+    );
+  });
+
+  //* Selecting an enrolled student to edit
+  $(document).on("click", ".student-table tbody tr", function () {
+    const rowId = $(this).attr("id");
+    const modalContent = editStudent(rowId);
+
+    $("#modalBloc").modal("toggle");
+    $("#edit-form")
+      .empty()
+      .append(modalContent)
+      .attr("data-bloc", "edit-student");
+  });
+
+  //* Event handler for end date input in modal form
+  //* when editing a student.
+  $(document).on("focusout", "#edit-form #InactiveDate-view", function (evnt) {
+    evnt.stopPropagation();
+    const endDate = $(this).val();
+    if (endDate) {
+      // console.log("it works", `==> ${endDate}`);
+      $("#InactiveReason-view, #TransferTo-view").prop("disabled", false);
+    } else {
+      // console.log("Nothing here!");
+      return;
+    }
   });
 });
