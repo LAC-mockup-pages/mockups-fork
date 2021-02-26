3c587c145fc395583522e5db2e5e2e25b296264b
Patrick_B <pb5191@gmail.com>
2021-02-26 16:09:01 -0500

    added table for student monthly hours

--- instructional-activity/scripts/components/hours/StudentHours.js
+++ instructional-activity/scripts/components/hours/StudentHours.js
@@ -2,7 +2,7 @@
 // Accessible only from the Enrollment and Details views.
 
 export const createStudentsBloc = (classId) => {
-  let bloc = "";
+  let body = "";
   const yearlyHours = GetContactHours_Annual.slice(0);
 
   const months = [
@@ -20,10 +20,30 @@ export const createStudentsBloc = (classId) => {
     "Jun."
   ];
 
-  for (const record in yearlyHours) {
-    for (const key of Object.keys(record)) {
+  for (const record of yearlyHours) {
+    const { ID, Class_PKID, Student_PKID, StudentName, BirthDate } = record;
+    let monthValues = "";
+
+    const monthlyHours = Object.keys(record).filter((field) =>
+      field.includes("Hours")
+    );
+
+    for (const key of monthlyHours) {
       const value = record[key];
+      monthValues += `
+      <td class="cell-data month-value">
+        <input class="cell-input" name=${key} value=${value}>
+      </td>
+      `;
     }
+
+    body += `
+    <tr id=${ID} data-class=${Class_PKID} data-student=${Student_PKID}>
+      <td class="cell-data student-name">${StudentName}</td>
+      <td class="cell-data dob">${BirthDate}</td>
+      ${monthValues}
+    </tr>
+    `;
   }
 
   // createHeaders() <== helperFunctions.js
@@ -31,13 +51,14 @@ export const createStudentsBloc = (classId) => {
 
   return `
   <div class="container-fluid row blue-light-text" id="student-hours">
-    <div class="student-title label-text">${classId}  -  Student Hours</div>
+    <div class="student-title label-text col-md-4">${classId}</div>
+    <div class="label-text col-md-2">Student Hours</div>
   </div>
   <form class="student-hours" role="form" id="student-hours-form">
-    <table class="table" id="student-hours-table">
+    <table class="table table-condensed" id="student-hours-table">
       ${header}
-      <tbody>
-
+      <tbody class="student-hours-body">
+        ${body}
       </tbody>
     </table>
   </form>
