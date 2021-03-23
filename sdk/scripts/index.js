4554a8d6c3927f9ce15b0a1f937dc735f3d6cbb2
Patrick_B <pb5191@gmail.com>
2021-03-23 11:13:08 -0400

    modified buildPeriods to output same format as Student periods

--- sdk/scripts/index.js
+++ sdk/scripts/index.js
@@ -1,20 +1,34 @@
 // Actions and logic
 
-const createObject = (list) => {
-  const resultObj = {};
-  for (const record of list) {
-    const { name, value } = record;
-    resultObj[name] = value;
+const testDate = () => {
+  const firstSemester = [];
+  const secondSemester = [];
+  for (let i = 6, j = 0; i < 12, j < 6; i++, j++) {
+    const date1 = new Date("2021", i, "01");
+    const date2 = new Date("2021", j, "01");
+
+    const month1 = `${date1.toDateString().substr(4, 3)}Hours`;
+    const month2 = `${date2.toDateString().substr(4, 3)}Hours`;
+
+    firstSemester.push(month1);
+    secondSemester.push(month2);
   }
-  return resultObj;
+  return [...secondSemester, ...firstSemester];
 };
 
-console.log(
-  makeObject([
-    { name: "New1", value: "Paul" },
-    { name: "New2", value: "Peter" },
-    { name: "New3", value: "John" },
-    { name: "New4", value: "Bill" },
-    { name: "New5", value: "Alfred" }
-  ])
-);
+const testDate2 = () => {
+  const createMonthList = (num) => {
+    return new Array(6).fill("Hours", 0, 6).map((item, indx) => {
+      const month = new Date("2021", indx + num, "01")
+        .toDateString()
+        .substr(4, 3);
+      return month + item;
+    });
+  };
+  const first = createMonthList(0);
+  const second = createMonthList(6);
+  return [...second, ...first];
+};
+
+// console.log("testDate() :>> ", testDate());
+console.log("testDate2() :>> ", testDate2());
