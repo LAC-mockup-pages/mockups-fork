8c44b66c60932f0a3ba2abe4e5f04309b3e40ead
Patrick_B <pb5191@gmail.com>
2021-04-20 11:46:39 -0400

    added modal links

--- log-in/scripts/index.js
+++ log-in/scripts/index.js
@@ -4,3 +4,24 @@
 //* =================================================
 //* jQuery section
 //* =================================================
+$(document).ready(function () {
+  //* Selected news article displayed in modal
+  $(document).on("click", "a.modalButton", function (evnt) {
+    const height = $(this).attr("data-height") || 300;
+    const width = $(this).attr("data-width") || 400;
+    const title = $(this).text();
+    console.log("title :>> ", this);
+    $("#myModal .modal-title").text(title);
+    const newsArticle = $(this).data("news");
+
+    // const characterCodes = newsArticle
+    //   .split("")
+    //   .map((char) => char.charCodeAt(0) + " " + char);
+
+    // console.table(characterCodes);
+    $("#myModal .modal-body #myModalTextBox").text(newsArticle);
+    $("body").on("hidden.bs.modal", "#myModal", function () {
+      $(this).removeData("bs.modal");
+    });
+  });
+});
