b1054a5fc481884fc6a722de82a9d0892afbf268
Patrick_B <pb5191@gmail.com>
2021-05-02 15:12:17 -0400

    added snippet for QUnit

--- instructional-activity/testing/main-test.js
+++ instructional-activity/testing/main-test.js
@@ -1,23 +1,17 @@
 //* Functions to test
 //*=================================
 
+const adding = (num1, num2) => {
+  // return num1 + num2;
+};
+
 //*=================================
 //* jQuery section
 //*=================================
 $(document).ready(() => {
-  QUnit.module("add", () => {
-    QUnit.test("should add two numbers", (assert) => {
-      assert.equal(adding(1, 1), 2);
-      assert.notEqual(adding(1, "1"), 2, "Failing test as 1 param is a string");
-    });
-  });
-
-  QUnit.module("setFiscalYear", () => {
-    QUnit.test("should return a string", (assert) => {
-      const testDate = "";
-
-      assert.ok(typeof setFiscalYear(testDate) === "string");
-      assert.equal(setFiscalYear(testDate), "2020", `${testDate} ==> "2020"`);
+  QUnit.module("adding", () => {
+    QUnit.test("should add something", (assert) => {
+      assert.equal(adding(1, 1), 2, "1+1 = 2");
     });
   });
 });
