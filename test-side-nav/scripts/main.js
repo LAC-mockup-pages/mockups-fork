fa74d468eddabbd6a1ff95d8db1d06fc0850ad8a
Patrick_B <pb5191@gmail.com>
2021-05-03 12:33:06 -0400

    added sub-menu dropdown to sidenav

--- test-side-nav/scripts/main.js
+++ test-side-nav/scripts/main.js
@@ -10,13 +10,22 @@ $(document).ready(() => {
   //* Opening side Nav
   $(document).on("click", "#menu-btn", function (evnt) {
     $(".sidenav").width("20%");
-    $(".sidenav a").toggleClass("hidden");
-    $(".sidenav .small-label").toggleClass("hidden");
+    $(
+      ".sidenav .main-tab, .sidenav .close-btn, .sidenav .small-label"
+    ).toggleClass("hidden");
   });
 
   //* Closing side Nav
   $(document).on("click", ".close-btn", function (evnt) {
     $(".sidenav").width("3%");
-    $(".sidenav a, .small-label").toggleClass("hidden");
+    $(".sidenav .main-tab, .sidenav .close-btn , .small-label").toggleClass(
+      "hidden"
+    );
+  });
+
+  //* Selecting a menu item and displaying the sub-menu
+  $(document).on("click", ".dropdown-btn", function (evnt) {
+    evnt.stopPropagation();
+    $(".dropdown-container").css("display", "block");
   });
 });
