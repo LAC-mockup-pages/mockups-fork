52a6e1f6f78ed04cf2373fa45b3cd7565507d857
Patrick_B <pb5191@gmail.com>
2021-05-04 09:55:05 -0400

    added closing side nav bar after selecting a sub menu item

--- test-side-nav/scripts/main.js
+++ test-side-nav/scripts/main.js
@@ -2,6 +2,12 @@
 //* Actions and Logic
 //*=================================================
 
+const toggleSideNav = () => {
+  $(
+    ".sidenav .main-tab, .sidenav .close-btn, .sidenav .small-label"
+  ).toggleClass("hidden");
+};
+
 //*=================================================
 //* jQuery section
 //*=================================================
@@ -10,22 +16,24 @@ $(document).ready(() => {
   //* Opening side Nav
   $(document).on("click", "#menu-btn", function (evnt) {
     $(".sidenav").width("20%");
-    $(
-      ".sidenav .main-tab, .sidenav .close-btn, .sidenav .small-label"
-    ).toggleClass("hidden");
+    toggleSideNav();
   });
 
   //* Closing sidenav by clicking close-btn or sidenav losing focus
   $(document).on("click", ".close-btn", function (evnt) {
     $(".sidenav").width("3%");
-    $(".sidenav .main-tab, .sidenav .close-btn , .small-label").toggleClass(
-      "hidden"
-    );
+    toggleSideNav();
   });
 
   //* Selecting a menu item and displaying the sub-menu
   $(document).on("click", ".dropdown-btn", function (evnt) {
-    evnt.stopPropagation();
     $(".dropdown-container").css("display", "block");
+  });
+
+  //* Selecting a submenu item
+  $(document).on("click", ".dropdown-container a", function (evnt) {
+    $(".dropdown-container").css("display", "none");
+    $(".sidenav").width("3%");
+    toggleSideNav();
   });
 });
