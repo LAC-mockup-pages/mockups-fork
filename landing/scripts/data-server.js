296c235ee19c49d5c870280f81a7023a595de42a
Patrick_B <pb5191@gmail.com>
2021-06-09 10:11:24 -0400

    added data object for Agency selection

---
+++ landing/scripts/data-server.js
@@ -0,0 +1,23 @@
+//* Data objects for Landing Page
+//*=================================
+
+const GetAgencyIndex = [
+  {
+    AgencyID: "PHIP",
+    AgencyName: "Phipps Community Development Corporation",
+    Division: ""
+  },
+  { AgencyID: "PWSD", AgencyName: "Port Washington UFSD", Division: "" },
+  {
+    AgencyID: "PRA",
+    AgencyName: "Practice Agency",
+    Division: "AAA Division Changes Made Dec 2 Venu"
+  },
+  { AgencyID: "PRA1", AgencyName: "Practice Agency 1", Division: "" },
+  { AgencyID: "PRY", AgencyName: "Project Reach Youth", Division: "" },
+  {
+    AgencyID: "PNWB",
+    AgencyName: "Putnam/Northern Westchester BOCES",
+    Division: ""
+  }
+];
