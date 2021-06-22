8ad589b721144850d784a408a24cd654a9ae8694
Patrick_B <pb5191@gmail.com>
2021-06-22 16:38:49 -0400

    added sample data objects

--- landing/scripts/data-server.js
+++ landing/scripts/data-server.js
@@ -21,3 +21,23 @@ const GetAgencyIndex = [
     Division: ""
   }
 ];
+
+const GetAgencyCardValues = [
+  { ID: "card0", values: ["20"], report: ["../404-page/index.html"] },
+  { ID: "card1", values: ["125"], report: [""] },
+  { ID: "card2", values: ["120"], report: ["../404-page/index.html"] },
+  { ID: "card3", values: ["24"], report: ["../404-page/index.html"] },
+  {
+    ID: "card4",
+    values: ["100", "101"],
+    report: ["../404-page/index.html", "../404-page/index.html"]
+  },
+  {
+    ID: "card5",
+    values: ["121", "16"],
+    report: ["../404-page/index.html", "../404-page/index.html"]
+  },
+  { ID: "card6", values: ["21%", "25%"], report: ["../404-page/index.html"] },
+  { ID: "card7", values: ["15%", "14%"], report: ["../404-page/index.html"] },
+  { ID: "card8", values: ["1240", "45"], report: ["../404-page/index.html"] }
+];
