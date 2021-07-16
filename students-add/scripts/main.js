d013b2c5aee402e070c323dc4b06b8d9d3e178c3
Patrick_B <pb5191@gmail.com>
2021-07-16 16:07:47 -0400

    added progress bar animation and reset

--- students-add/scripts/main.js
+++ students-add/scripts/main.js
@@ -2,6 +2,20 @@
 //* Actions and Logic for local page
 //*=================================================
 
+const increaseBar = (elem) => {
+  let width = 1;
+  const id = setInterval(frame, 400);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+      $("#bar-container").toggleClass("hidden");
+    } else {
+      width += Math.floor(Math.random() * 10 + 1);
+      elem.width(`${width}%`);
+    }
+  }
+};
+
 //*=================================================
 //* jQuery section
 //*=================================================
@@ -9,6 +23,12 @@
 $(document).ready(() => {
   //* Send focus on first name input
   $("#first-name").focus();
+
+  //* Removes hidden class on closing modal
+  $("#close-button").click(() => {
+    $("#bar-container").toggleClass("hidden");
+    $("#modalBloc").modal("toggle");
+  });
 
   //* Triggers search for possible duplicates after first, middle,
   //* last and DOB are entered
@@ -30,18 +50,23 @@ $(document).ready(() => {
         StudentID: "AdamsAlbertPRA2252017111981",
         First: "Albert",
         Middle: "D.",
-        Last: "Adams"
+        Last: "Adams",
+        BirthDate: "01/01/1981"
       },
       {
         ID: "02",
         StudentID: "AdamAlanPRA2252018111982",
         First: "Alan",
         Middle: "D.",
-        Last: "Adam"
+        Last: "Adam",
+        BirthDate: "01/01/1982"
       }
     ];
     //! ==========================================
 
     $("#modalBloc").modal("toggle");
+    const element = $("#load-bar");
+    $("#bar-container").removeClass("hidden");
+    increaseBar(element);
   });
 });
