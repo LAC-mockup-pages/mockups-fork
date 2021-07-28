74cc2dd741fe95ee979f30591342a08168bb2167
Patrick_B <pb5191@gmail.com>
2021-07-28 11:50:01 -0400

    added file for saving data

---
+++ students-add/scripts/components/data-save.js
@@ -0,0 +1,20 @@
+//* Saving data
+//* ===========================
+
+// export const initialSave = async (saveObj) => {
+//   try {
+//     const body = JSON.stringify(saveObj);
+//     const response = await fetch("MyURL", { method: "POST", body });
+//     return response.json()
+//   } catch (error) {
+//     console.log(`Something went wrong! ${error}`);
+//   }
+// };
+
+//! ==================================================
+//! Only for development - Comment out for production
+//! ==================================================
+
+export const initialSave = () => {
+  return { ID: "0000009" };
+};
