f96b46e28219437a503b30377cc91472ea962a3a
Patrick_B <pb5191@gmail.com>
2021-08-24 10:32:58 -0400

    added selection from list

--- students-landing/scripts/main.js
+++ students-landing/scripts/main.js
@@ -2,7 +2,23 @@
 //* Actions and Logic for local page
 //*=================================================
 
-const createDataList = (inputString) => {};
+const createDataList = (inputString) => {
+  const result = [];
+  const studentList = GetStudentID.filter((student) =>
+    student.Last.toLowerCase().startsWith(inputString.toLowerCase())
+  ).sort((student1, student2) =>
+    student1.Last < student2.Last ? -1 : student1.Last > student2.Last ? 1 : 0
+  );
+
+  for (const student of studentList) {
+    const { ID, StudentID, First, Middle, Last } = student;
+    result.push(
+      `<option value="${Last}, ${First} ${Middle} | ${StudentID}" data-id=${ID}/>`
+    );
+  }
+
+  return result;
+};
 
 //*=================================================
 //* jQuery section
@@ -17,13 +33,19 @@ $(document).ready(() => {
   $("#search-input").focus();
 
   //* User input first 3 letters
-  $("#search-input").change((evnt) => {
+  $("#search-input").keyup((evnt) => {
     evnt.stopPropagation();
     evnt.preventDefault();
-    const input = $(this).val();
+    console.log("Starting!");
+    const input = $("#search-input").val();
+    console.log("input :>> ", input);
+
     if (input.length > 2) {
       const datalist = createDataList(input);
       $("#students").append(datalist);
+      $("#students").click();
     }
   });
+
+  //* Search button is clicked or pressed
 });
