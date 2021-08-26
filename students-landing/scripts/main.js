3179ef8c20c5f23a4de7712b28d1617b2ac81e18
Patrick_B <pb5191@gmail.com>
2021-08-26 13:28:13 -0400

    added toggling hidden on click

--- students-landing/scripts/main.js
+++ students-landing/scripts/main.js
@@ -60,5 +60,7 @@ $(document).ready(() => {
 
     console.log(selection);
     console.log("optionList :>> ", optionList);
+
+    $(".student-search, .sub-navbar, .student-display").toggleClass("hidden");
   });
 });
