a3f21a2695f29f9d731219f398f94096724bf4ca
Patrick_B <pb5191@gmail.com>
2021-09-03 12:45:31 -0400

    added request object creation from URL with query string

--- students-landing/scripts/main.js
+++ students-landing/scripts/main.js
@@ -2,6 +2,17 @@
 //* Actions and Logic for local page
 //*=================================================
 
+const createCredentials = () => {
+  const AgencyID = SESSION_VARIABLE[0].AgencyID.startsWith("<%= Session")
+    ? "PRA"
+    : SESSION_VARIABLE[0].AgencyID;
+  const UserID = SESSION_VARIABLE[0].AuditUserID.startsWith("<%= Session")
+    ? "0000001"
+    : SESSION_VARIABLE[0].AuditUserID;
+
+  return { AgencyID, UserID };
+};
+
 //*=================================================
 //* jQuery section
 //*=================================================
@@ -10,4 +21,12 @@ $(document).ready(() => {
   //* =====================================
   //* At first rendering
   //* =====================================
+  const currentUrl = new URL(window.location.href);
+  const params = new URLSearchParams(currentUrl.search);
+  const Student_PKID = params.get("st_id");
+  const credentials = createCredentials();
+
+  const requestObj = [{ ...credentials, Student_PKID }];
+
+  console.log("requestObj :>> ", requestObj);
 });
