c27e10d56c0c8471534c4f6d9a2ccf518c4101cb
Patrick_B <pb5191@gmail.com>
2021-09-17 12:15:09 -0400

    modified alignment

--- students-profile/scripts/components/exit.js
+++ students-profile/scripts/components/exit.js
@@ -2,8 +2,7 @@
 //* =====================================================
 
 export const exitSection = (dataObj) => {
-  let { LastActiveMonth, Q2_Outcome, Q4_Outcome, HSE_Outcome } = dataObj;
-
+  const { LastActiveMonth, Q2_Outcome, Q4_Outcome, HSE_Outcome } = dataObj;
   const valueList = [LastActiveMonth, Q2_Outcome, Q4_Outcome, HSE_Outcome].map(
     (item) => {
       return item === "null" ? "Not Applicable" : item;
@@ -12,10 +11,9 @@ export const exitSection = (dataObj) => {
   const sectionContent = `
 <div class="box-title">Exit status</div>
 <div class="row">
-<div class="col-md-5 rubrique">Last month of attendance:</div>
-<div class="col-md-7">${valueList[0]}</div>
+  <div class="col-md-5 rubrique">Last month of attendance:</div>
+  <div class="col-md-7">${valueList[0]}</div>
 </div>
-
 <div class="row">
   <div class="col-md-5 rubrique">Employment Q2:</div>
   <div class="col-md-7">${valueList[1]}</div>
@@ -24,7 +22,6 @@ export const exitSection = (dataObj) => {
   <div class="col-md-5 rubrique">Employment Q4:</div>
   <div class="col-md-7">${valueList[2]}</div>
 </div>
-
 <div class="row">
   <div class="col-md-5 rubrique">HSE outcome:</div>
   <div class="col-md-7">${valueList[3]}</div>
