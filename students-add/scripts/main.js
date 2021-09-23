d1b568b13e382c2fc41784fde974c62c3911653b
Patrick_B <pb5191@gmail.com>
2021-09-23 10:55:34 -0400

    removed duplicate code

--- students-add/scripts/main.js
+++ students-add/scripts/main.js
@@ -48,24 +48,6 @@ const formatSSNWithDashes = (ssn) => {
   return val.substring(0, 11);
 };
 
-const formatPhoneWithDashes = (phone) => {
-  // remove all non-dash and non-numerals
-  let val = phone.replace(/[^\d-]/g, "");
-  // add the first dash if number from the second group appear
-  val = val.replace(/^(\d{3})-?(\d{1,2})/, "$1-$2");
-  // add the second dash if numbers from the third group appear
-  val = val.replace(/^(\d{3})-?(\d{3})-?(\d{1,4})/, "$1-$2-$3");
-  // remove misplaced dashes
-  val = val
-    .split("")
-    .filter((val, idx) => {
-      return val !== "-" || idx === 3 || idx === 7;
-    })
-    .join("");
-  // enforce max length
-  return val.substring(0, 12);
-};
-
 const createOptionList = (dataObj, defaultValue) => {
   const optionList = dataObj.map((record) => {
     const [key, value] = Object.keys(record);
