c71c719f7fea7e66aa0394a52349928681156979
Patrick_B <pb5191@gmail.com>
2021-10-18 11:58:53 -0400

    modified parameter name

--- students-details/scripts/components.js/student-info.js
+++ students-details/scripts/components.js/student-info.js
@@ -8,32 +8,32 @@ import { dateFormat, createOptionList, createStaffList } from "../main.js";
 
 // Populating input element values
 // Data source: original-data/student-data.js/GetStudent
-export const inputValues = (list) => {
+export const inputValues = (student) => {
   $("form input").each(function () {
     const name = $(this).attr("name");
     const value =
       $(this).attr("type") === "date" && name !== "DateSettled"
-        ? dateFormat(list[name])
-        : list[name];
+        ? dateFormat(student[name])
+        : student[name];
     $(this).val(value);
   });
 };
-// Populating Work Eligibility section select values. Adding option list.
+// Populating Work Eligibility section select values. Adding option student.
 // Data source: original-data/student-data.js/GetStudent,
 //        data-server.js/GetStaff
-export const ssnValues = (list) => {
-  $(".ssn-form select[name='NoSSNVisa']").val(list.NoSSNVisa);
+export const ssnValues = (student) => {
+  $(".ssn-form select[name='NoSSNVisa']").val(student.NoSSNVisa);
   const optionStaff = createOptionList(
     createStaffList(GetStaff),
-    list.NoSSNVisaStaff
+    student.NoSSNVisaStaff
   );
   $(".ssn-form select[name='NoSSNVisaStaff']").append(optionStaff);
 };
 // Populating Gender | Ethnicity section select values.
 // Data source: original-data/student-data.js/GetStudent,
 //        data-server.js/ddlRace
-export const genderValues = (list) => {
-  $(".gender-form select[name='Sex']").val(list.Sex);
-  $(".gender-form select[name='EthnicityID']").val(list.EthnicityID);
+export const genderValues = (student) => {
+  $(".gender-form select[name='Sex']").val(student.Sex);
+  $(".gender-form select[name='EthnicityID']").val(student.EthnicityID);
   //TODO RaceID multiple selects
 };
