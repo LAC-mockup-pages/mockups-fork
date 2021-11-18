3c8beba24458692e029fbe10ee5db10767defd27
Patrick_B <pb5191@gmail.com>
2021-11-18 18:01:56 -0500

    modified event + triggered modal

--- students-assessments/scripts/main.js
+++ students-assessments/scripts/main.js
@@ -14,9 +14,17 @@ $(document).ready(() => {
   //* =====================================
 
   //* Expand underpinned table when section is clicked
-  $("section").click(function (evnt) {
+  $(".sub-header").click(function (evnt) {
     evnt.stopPropagation();
     $("section .visible").removeClass("visible").addClass("invisible");
-    $(".invisible", this).removeClass("invisible").addClass("visible");
+    $(this).siblings(".invisible").removeClass("invisible").addClass("visible");
+  });
+
+  //* Triggers edit modal with selected row elements and values
+  $(document).on("click", ".table>tbody> tr", function (evnt) {
+    evnt.stopPropagation();
+    const editFormContent = "<h2>Selected Row</h2>";
+    $("#modalBloc").modal("toggle");
+    $("#edit-form").empty().append(editFormContent);
   });
 });
