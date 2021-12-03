17ca26fa55177004059f1790868bcc93833f0658
Patrick_B <pb5191@gmail.com>
2021-12-03 16:08:36 -0500

    removed comments

--- students-details/scripts/main.js
+++ students-details/scripts/main.js
@@ -152,7 +152,6 @@ $(document).ready(() => {
   ssnValues(studentInfo);
   genderValues(studentInfo);
   fundingValues(fundingInfo, fundingSources);
-
   populationValues(additionalInfo);
   barriersValues(studentInfo);
   assistanceValues(assistanceInfo, assistanceSource);
