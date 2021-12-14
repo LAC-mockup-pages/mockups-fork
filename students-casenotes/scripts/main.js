3380d560e156e1753492ef063c80ff1ff097e3ef
Patrick_B <pb5191@gmail.com>
2021-12-14 12:25:21 -0500

    added nav and generic process

--- students-casenotes/scripts/main.js
+++ students-casenotes/scripts/main.js
@@ -2,8 +2,65 @@
 //* Actions and Logic for local page
 //*=================================================
 
+//! =============================================================
+//! For Development only.
+//! Comment out for Production.
+//! =============================================================
+const studentInfo = GetStudentHeader.slice(0)[0];
+// const wioaOutcome = GetOutcomeinfo_WIOA.slice(0);
+// const credentialList = GetCredentialSource.slice(0);
+// const outcomeList = GetOutcomeDesc_WIOA.slice(0);
+//! =============================================================
+
 //*=================================================
 //* jQuery section
 //*=================================================
 
-$(document).ready(() => {});
+$(document).ready(() => {
+  //* =====================================
+  //* At first rendering
+  //* =====================================
+  const currentUrl = new URL(window.location.href);
+  const params = new URLSearchParams(currentUrl.search);
+  // Default Student_PKID if no query string in URL
+  const Student_PKID = params.get("stid") ? params.get("stid") : "1048303";
+
+  //! =============================================================
+  //! For Production, this section regroups the end points
+  //! for the different requests to the back-end, using requestObj.
+  //! Navigation variables are defined here as well.
+  //! =============================================================
+  // const studentInfo =
+  // const wioaOutcome =
+  // const credentialList =
+  // const outcomeList =
+
+  // Navigation variables.
+  // In Production, update with actual rootUrl and destinations
+  // once they are known.
+  const rootUrl = "http://localhost:5500/";
+  const destinationsObj = {
+    profile: "student-profile/index.html",
+    details: "students-details/index.html",
+    assessments: "student-assessments/index.html",
+    enrollment: "student-enrollment/index.html",
+    goals: "student-goals/index.html"
+  };
+  //! =============================================================
+
+  //* Navigation from sub navbar on top
+  $("#sub-nav li").click(function (evnt) {
+    evnt.stopPropagation();
+    $("#sub-nav li").removeClass("blue-light-bg blue-text");
+    $(this).toggleClass("blue-light-bg blue-text");
+    const queryString = `?stid=${Student_PKID}`;
+    const destination = $(this).attr("data-target");
+    const targetUrl = `${rootUrl}${destinationsObj[destination]}${queryString}`;
+    window.location.assign(targetUrl);
+  });
+
+  // Populating student name and birthdate
+  const { BirthDate, StudentName } = studentInfo;
+  $(".name").text(StudentName);
+  $(".dob").text(BirthDate);
+});
