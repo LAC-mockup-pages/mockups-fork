97b24bf966dec78bef175e6cf7a3b364a06fa92a
Patrick_B <pb5191@gmail.com>
2021-12-26 16:00:10 -0500

    Add row creation in modal for Add New Record

--- students-enrollment/scripts/components/enrollment.js
+++ students-enrollment/scripts/components/enrollment.js
@@ -109,67 +109,76 @@ export const addNewRecord = (courses, reasons, transfers, caseFlag = "0") => {
     InactiveDate: "Left",
     InactiveReason: "Reason",
     Transfer_PKID: "Transfer to",
-    ActiveStatus: "Active"
+    ActiveStatus: "Active",
+    ISCMP: "ISCMP"
   };
   const content = [];
   const ISCMP = caseFlag;
   // createCredentials() ==> helpers/helperFunctions.js
   const { FiscalYear } = createCredentials();
-  const optionListCourse = createOptionList(courses);
-  const optionListReason = createOptionList(reasons);
-  const optionListTransfer = createOptionList(transfers);
   const optionListActive = createOptionList([
     { key: "0", value: "No" },
     { key: "checked", value: "Yes" }
   ]);
-  for (const key in labels) {
-    const labelText = labels[key];
-    let rowContent = "";
+  let labelClassVal = "";
+  let classVal = "";
 
-    switch (key) {
-      case "DescriptionKey":
-        rowContent = `
-        <select class="modal-select name=${key}>
-          <option value>Select a course</option>
-          ${optionListCourse}
-        </select>
-        `;
-        break;
-      case "InactiveReason":
-        rowContent = `
-          <select class="modal-select name=${key}>
-            <option value>Select a reason</option>
-            ${optionListReason}
-          </select>
-        `;
-        break;
-      case "Transfer_PKID":
-        rowContent = `
-        <select class="modal-select name=${key}>
-          <option value>Select a transfer</option>
-          ${optionListTransfer}
-        </select>
-      `;
-        break;
-      case "ActiveStatus":
-        rowContent = `
-        <select class="modal-select name=${key}>
-          <option value>Select a status</option>
-          ${optionListActive}
-        </select>
-      `;
-        break;
-      default:
-        rowContent = `
-      <input type="date" name=${key} value >`;
-        break;
+  for (const keyValue in labels) {
+    const labelVal = labels[keyValue];
+    let row = "";
+    const value = keyValue === "FY" ? FiscalYear : "";
+    const option = keyValue === "FY" ? "disabled" : "";
+
+    if (["FY", "EnrollDate", "InactiveDate", "ISCMP"].includes(keyValue)) {
+      // Input fields
+      const optionHidden =
+        keyValue === "ISCMP" ? "input-group hidden" : "input-group";
+      const type = keyValue.includes("Date") ? "date" : "text";
+      // elementInput() ==> helpers/helperFunctions.js
+      row = elementInput({
+        keyVal: keyValue,
+        labelVal,
+        value,
+        labelClassVal,
+        classVal,
+        option,
+        optionHidden,
+        type
+      });
+    } else {
+      // Select fields: DescriptionKey, InactiveReason, Transfer_PKID,
+      // ActiveStatus
+      let hashTable = [];
+
+      switch (keyValue) {
+        case "DescriptionKey":
+          hashTable = courses;
+          break;
+        case "InactiveReason":
+          hashTable = reasons;
+          break;
+        case "Transfer_PKID":
+          hashTable = transfers;
+          break;
+        default:
+          hashTable = [
+            { key: "0", value: "No" },
+            { key: "checked", value: "Yes" }
+          ];
+          break;
+      }
+      // elementSelectModal() ==> helpers/helperFunction.js
+      row = elementSelectModal({
+        hashTable,
+        keyValue,
+        selectedValue: "",
+        labelVal,
+        labelClassVal: "",
+        option: "",
+        optionText: ""
+      });
     }
-    const row = `
-    <div class="form-group input-field">
-      <label for=${key}>${labelText}></label>
-      ${rowContent}
-    </div>
-    `;
+
     content.push(row);
   }
   const editFormContent = content.join("");
