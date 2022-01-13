8068edea5525e9ae82008dabec77df0f530feb8c
Patrick_B <pb5191@gmail.com>
2022-01-13 09:32:09 -0500

    add save process after editing

--- students-goals/scripts/main.js
+++ students-goals/scripts/main.js
@@ -166,7 +166,6 @@ $(document).ready(() => {
     evnt.preventDefault();
     const rowId = $(this).attr("id");
     const $row = $(":input", this).clone().prop("disabled", false);
-
     const sectionTitle = $(this)
       .parents("section")
       .find(".sub-header-title")
@@ -219,5 +218,49 @@ $(document).ready(() => {
     }
     // Enables customized tooltips
     $("[data-toggle='tooltip']").tooltip();
+  });
+
+  //* Saving changes after editing in modal
+  $("#save-btn").click(function (evnt) {
+    //TODO Check for errors in inputs
+    const elements = $("#edit-form :input").prop("disabled", false);
+    const requiredObj = createObject(
+      $("#edit-form [required]").serializeArray()
+    );
+    const requiredWithoutValue = [];
+    // Checking if required fields have a valid value.
+    // Switching background color to yellow if not, and stopping
+    // the save process.
+    for (const key in requiredObj) {
+      if (requiredObj[key].length < 1) requiredWithoutValue.push(key);
+    }
+    if (requiredWithoutValue.length > 0) {
+      for (const field of requiredWithoutValue) {
+        $(`.modal-body [name=${field}]`).css("background-color", "#f7e095");
+      }
+      return;
+    } else {
+      const targetTable = $("#edit-form").attr("data-table");
+      const saveList = $(elements).serializeArray();
+      // createObject() <== helpers/helperFunctions.js
+      let saveObj = createObject(saveList);
+
+      // Adding ID of edited record if it exists.
+      const rowId = $("#edit-form").attr("data-id");
+      if (rowId) saveObj = { ID: rowId, ...saveObj };
+      const credentials = createCredentials();
+      if (saveObj.ActiveStatus === "0") saveObj.ActiveStatus = "";
+      //! =================================================
+      //! For production, this is the end point for the Post request
+      //! to update the DB.
+      //! =================================================
+      const resultList = [
+        targetTable,
+        JSON.stringify({ ...credentials, Student_PKID, ...saveObj })
+      ];
+      console.log("result :", resultList);
+      //! =================================================
+      $("#modalBloc").modal("toggle");
+    }
   });
 });
