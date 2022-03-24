2f7cce323523d74f07f7fbfb020ef79bf072ddbc
Patrick_B <pb5191@gmail.com>
2022-03-24 13:53:43 -0400

    Add data processing for generate-report

--- reports-funding/scripts/button-events/generate-event.js
+++ reports-funding/scripts/button-events/generate-event.js
@@ -1,6 +1,19 @@
 //* Process after generate-btn is clicked
 //* =====================================
 
-export const createReport = () => {
+export const createReport = (reportObj) => {
   console.log("Report created in new browser tab");
+  // createObject() <= helpers/helperFunctions.js
+  const selectedValues = createObject($(".selectors").serializeArray());
+  console.log(
+    "🚀 / file: generate-event.js / line 7 / createReport / selectedValues",
+    selectedValues
+  );
+  // createCredentials <= helpers/helperFunctions.js
+  const { AgencyID } = createCredentials();
+
+  const { fiscalYear, classFunding, reportCategory } = selectedValues;
+  const { FileName } = reportObj;
+  const startDate = `07/01/${fiscalYear}`;
+  const endDate = `06/30/${fiscalYear}`;
 };
