d3750c31af6f5d122dfaf6329301ae50bbb3e141
Patrick_B <pb5191@gmail.com>
2022-04-15 16:00:26 -0400

    Add new data object for card values

--- landing/scripts/data-server.js
+++ landing/scripts/data-server.js
@@ -53,3 +53,87 @@ const GetAgencyCardValues = [
     report: ["../404-page/index-test.html"]
   }
 ];
+
+const GetAgencyCardValues2 = [
+  {
+    AgencyID: "SESD",
+    CardID: "card0",
+    CardValue: "185",
+    CardValue2: "",
+    CardReport:
+      "../DMR/Instructional Offering Reports/programIF.aspx?ag=SESD&cid=&fy=2022&fsid=& nfy=1",
+    CardReport2: "NULL"
+  },
+  {
+    AgencyID: "SESD",
+    CardID: "card1",
+    CardValue: "1904",
+    CardValue2: "",
+    CardReport: "NULL",
+    CardReport2: "NULL"
+  },
+  {
+    AgencyID: "SESD",
+    CardID: "card2",
+    CardValue: "1904",
+    CardValue2: "",
+    CardReport:
+      "../DMR/Instructional Offering Reports/IOR/roster.aspx?ag=SESD&cid=&fy=2022&fsid=& nfy=1",
+    CardReport2: "NULL"
+  },
+  {
+    AgencyID: "SESD",
+    CardID: "card3",
+    CardValue: "764",
+    CardValue2: "",
+    CardReport:
+      "../DMR/Instructional Offering Reports/IOH/ContactHoursSummary.aspx?ag=SESD&cid=&fy=2022&fsid=& nfy=1",
+    CardReport2: "NULL"
+  },
+  {
+    AgencyID: "SESD",
+    CardID: "card4",
+    CardValue: "204",
+    CardValue2: "204",
+    CardReport:
+      "../POP_DataCheckReports/WIOA_NoPreTest.aspx?st=07/01/2021&en=06/30/2022&ag=SESD&nfc=0&cf=2NYS&nfy=1",
+    CardReport2:
+      "../POP_DataCheckReports/WIOA_NoPreTest.aspx?st=07/01/2021&en=06/30/2022&ag=SESD&nfc=0&cf=L&nfy=1"
+  },
+  {
+    AgencyID: "SESD",
+    CardID: "card5",
+    CardValue: "520",
+    CardValue2: "499",
+    CardReport:
+      "../POP_DataCheckReports/WIOA_NoPostTest.aspx?st=07/01/2021&en=06/30/2022&ag=SESD&nfc=0&cf=2NYS&nfy=1",
+    CardReport2:
+      "../POP_DataCheckReports/WIOA_NoPostTest.aspx?st=07/01/2021&en=06/30/2022&ag=SESD&nfc=0&cf=L&nfy=1"
+  },
+  {
+    AgencyID: "SESD",
+    CardID: "card6",
+    CardValue: "29.0",
+    CardValue2: "37.0",
+    CardReport:
+      "../POP_Reports/POPProgramEval.aspx?st=07/01/2021&en=06/30/2022&ag=SESD&nfc=0&cf=2NYS&nfy=1",
+    CardReport2: "NULL"
+  },
+  {
+    AgencyID: "SESD",
+    CardID: "card7",
+    CardValue: "NULL",
+    CardValue2: "NULL",
+    CardReport:
+      "../NYRS_Reports/NYRSProgramEval.aspx?st=07/01/2021&en=06/30/2022&ag=SESD&nfc=0&cf=2NYS&nfy=1",
+    CardReport2: "NULL"
+  },
+  {
+    AgencyID: "SESD",
+    CardID: "card8",
+    CardValue: "95732.5",
+    CardValue2: "1610",
+    CardReport: "NULL",
+    CardReport2: "NULL"
+  }
+];
