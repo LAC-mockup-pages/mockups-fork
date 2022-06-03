78a6bfb2035b1e7b4fd14e2633dc7ca071932f2a
Patrick_B <pb5191@gmail.com>
2022-06-03 11:17:37 -0400

    Delete commented out code

--- reports-management/scripts/event-handlers/generate-event.js
+++ reports-management/scripts/event-handlers/generate-event.js
@@ -15,58 +15,9 @@
 // Format for Export to Excel URI:
 // ../reports/ReportExport.aspx" + "?st=" + startdate + "&en=" + enddate + "&ag=" + agencyid + "&cf=" + fundingselectedvalues +  "&nfc=" + criteriakey + "&tfc=" + criteriaselectedvalues + "&nfy=1" + "&qid=" + qid;
 
-// export const createReportURI = (reportObj, exportFlag = false) => {
-//   // createObject() <= helpers/helperFunctions.js
-//   const selectedValues = createObject($(".selectors").serializeArray());
-//   const getFundingSources = (selection, sourceObj) => {
-//     let sources = [];
-//     if (selection === "ALL") {
-//       $("#class-funding > option").each(function (indx) {
-//         const source = $(this).val();
-//         sources.push(source);
-//       });
-//     } else {
-//       for (const prop in sourceObj) {
-//         sources.push(sourceObj[prop]);
-//       }
-//     }
-//     return sources.join(",");
-//   };
-//   const getSelectedCriteria = () => {
-//     let list = $(".criteria select").serializeArray();
-//     list = list.filter((record) => record.value).map((record) => record.value);
-//     console.log("list :>> ", list);
-//     return list.join(",");
-//   };
-//   const selectedFunding = Object.keys(selectedValues).filter((key) =>
-//     key.startsWith("funding")
-//   );
-//   const sourceObj = {};
-//   for (const source of selectedFunding) {
-//     if (selectedValues[source]) sourceObj[source] = selectedValues[source];
-//   }
-//   // createCredentials <= helpers/helperFunctions.js
-//   const { AgencyID } = createCredentials();
-//   const { fiscalYear, funding, reportCategory } = selectedValues;
-//   const { ID, FileName } = reportObj;
-//   // exportFlag only has a truesy value when the Export button is clicked.
-//   // It modifies the pageName and add the report ID (qid) to the query string.
-//   const pageName = exportFlag ? "ReportExport.aspx" : FileName;
-//   const reportID = exportFlag ? `&qid=${ID}` : "";
-//   const fundingStr = getFundingSources(funding, sourceObj);
-//   const startDate = `07/01/${fiscalYear}`;
-//   const endDate = `06/30/${fiscalYear}`;
-//   const criteriaStr = getSelectedCriteria();
-//   const numFiscalYear = $("#fiscal-year").prop("selectedIndex");
-//   const reportURI = `../reports/${pageName}?st=${startDate}&en=${endDate}%ag=${AgencyID}&fc=${fundingStr}&nfc=${reportCategory}&tfc=${criteriaStr}&nfy=${numFiscalYear}${reportID}
-//   `;
-//   return reportURI;
-// };
-
-// Needs selectedValues
-
 // Initializing Luxon DateTime class for the module
 const DT = luxon.DateTime;
+
 const setNFYvalue = (selectedFY) => {
   // createCredentials() <== helpers/helperFunctions.js
   const { FiscalYear } = createCredentials();
