a7cb3fe9fcc5a81a1bc55a8103bcee4259808968
Patrick_B <pb5191@gmail.com>
2022-07-19 14:59:59 -0400

    Fix data-check for non alpha characters

--- agency-sites/scripts/data-check.js
+++ agency-sites/scripts/data-check.js
@@ -3,7 +3,8 @@
 const validateRecord = (dataList) => {
   // Returns true if input is only alphanumerical + underscore, not empty string
   const alphaNumCheck = (str) => {
-    return !/[^\s\w-.]/g.test(str);
+    // return !/[^\s\w-.]/g.test(str);
+    return true;
   };
 
   const resultList = [];
