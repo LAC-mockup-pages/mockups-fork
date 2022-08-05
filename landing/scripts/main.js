7b7cb7b2381d71c380864c0d9410e140611a0cb2
Patrick_B <pb5191@gmail.com>
2022-08-05 16:24:14 -0400

    Add redesigned top bar

--- landing/scripts/main.js
+++ landing/scripts/main.js
@@ -27,13 +27,6 @@ const applyColor = (colorStr) => {
   $(".large-num").css("color", colorStr);
   $(".card-block").css("border-color", colorStr);
 };
-
-const toggleSideNav = () => {
-  $(
-    ".sidenav .main-tab, .sidenav .close-btn, .sidenav .menu-text, .small-logo"
-  ).toggleClass("hidden");
-};
-
 // Customizing the card content
 const addCustomContent = (list) => {
   for (const obj of list) {
@@ -48,7 +41,6 @@ const addCustomContent = (list) => {
     }
   }
 };
-
 let slideIndex = 1;
 const showSlides = (num) => {
   const slides = $(".cards");
@@ -63,7 +55,6 @@ const showSlides = (num) => {
     .show(600)
     .addClass("visible");
   $(dots[slideIndex - 1]).addClass("active");
-
   // Calculate the top margin necessary to center the card inside
   // card-block
   const blockHeight = $(".card-block").height();
@@ -71,7 +62,6 @@ const showSlides = (num) => {
   const lineHeight = $(".dot").height() / 2;
   const addedMargin = (blockHeight - cardHeight) / 2 - lineHeight;
   $(slides[slideIndex - 1]).css("margin-top", `${addedMargin}px`);
-
   applyColor(cardColors[slideIndex]);
 };
 
@@ -139,47 +129,6 @@ $(document).ready(() => {
 
   //! =============================================================
 
-  //* Side nav open at home page loading
-  $(".sidenav").width("20%");
-  toggleSideNav();
-
-  //* Opening side Nav
-  $(document).on("click", "#menu-btn", function (evnt) {
-    $(".sidenav").width("20%");
-    $(".card-btn").css("padding-left", "31%");
-    $(".btn-numbers").css("margin-left", "10%");
-
-    toggleSideNav();
-  });
-  // Enables customized tooltips
-  $("[data-toggle='tooltip']").tooltip();
-
-  //* Add user info.
-  let { fullname, rolename, UserLevel, AgencyName, PrevAgency } =
-    SESSION_VARIABLE[0];
-
-  //! =========================================
-  //! For Dev Env only. Can stay for Production.
-  //! =========================================
-
-  if (!AgencyName || AgencyName.startsWith("<%=")) {
-    fullname = "Kate Tornese (default)";
-    UserLevel = "1";
-    // rolename = "Program Data Editor Program Data Editor ";
-    rolename = "LAC TECH Support";
-    AgencyName = "Practice Agency";
-  }
-  //! =========================================
-  const welcomeLine = `
-    <div class="row">
-      <div class="col-sm-1"></div>
-      <div class="welcome-text col-sm-7">Hello ${fullname} (${AgencyName})
-      </div>
-      <div class="role-text col-sm-4" data-level=${UserLevel}>User Role: ${rolename}
-      </div>
-    </div>`;
-  $(".user-info").append(welcomeLine);
-
   //* Update card content with custom values and links.
   const cardList = GetAgencyCardValues.map((record) => {
     let { CardID, CardValue, CardValue2, CardReport, CardReport2 } = record;
@@ -242,35 +191,6 @@ $(document).ready(() => {
 
   //* ===================================
 
-  //* Closing sidenav by clicking close-btn or sidenav losing focus
-  $(document).on("click", ".close-btn", function (evnt) {
-    $(".dropdown-container").css("display", "none");
-    $(".main-tab .dropdown-btn").removeClass("active");
-    $(".sidenav").width("3%");
-    $(".card-btn").css("padding-left", "15%");
-    $(".btn-numbers").css("margin-left", "-17%");
-    toggleSideNav();
-  });
-  //* Selecting a menu item and displaying the sub-menu
-  $(document).on("click", ".dropdown-btn", function (evnt) {
-    $(".dropdown-container").css("display", "none");
-    $(".main-tab .dropdown-btn").removeClass("active");
-    $(this).siblings(".dropdown-container").css("display", "block");
-    $(this).toggleClass("active");
-  });
-  //* Selecting a submenu item
-  $(document).on("click", ".dropdown-container a", function (evnt) {
-    $(".dropdown-container").css("display", "none");
-    $(".main-tab .dropdown-btn").removeClass("active");
-    $(".sidenav").width("3%");
-    toggleSideNav();
-  });
-  //* Selecting another page in subnav bar
-  $("#sub-nav li").on("click", function (evnt) {
-    evnt.stopPropagation();
-    $("#sub-nav li").removeClass("blue-light-bg blue-text");
-    $(this).toggleClass("blue-light-bg blue-text");
-  });
   //* Display next or previous card when a chevron is clicked on
   $(document).on("click", ".prev, .next", function (evnt) {
     evnt.stopPropagation();
